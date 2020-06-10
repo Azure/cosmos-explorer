1ca8566e15c07a46e9e2cd635ae272db901d6073
Steve Faulkner <stfaul@microsoft.com>
2020-06-10 15:53:29 -0500

    WIP

--- cypress/cleanup.js
+++ cypress/cleanup.js
@@ -1,11 +1,26 @@
 // Cleans up old databases from previous test runs
 const { CosmosClient } = require("@azure/cosmos");
 
+// TODO: Add support for other APIs
+const mongoRegex = RegExp("mongodb://.*:(.*)@(.*).mongo.cosmos.azure.com");
+const sqlRegex = RegExp("AccountEndpoint=https://(.*).documents.azure.com");
+
 async function cleanup() {
   const connectionString = process.env.CYPRESS_CONNECTION_STRING;
   if (!connectionString) {
     throw new Error("Connection string not provided");
   }
+
+  let client;
+  switch (connectionString) {
+    case sqlRegex.test(sqlRegex):
+      break;
+
+    default:
+      throw new Error("Unable to extract account name and key from connection string");
+      break;
+  }
+
   const client = new CosmosClient(connectionString);
 
   const response = await client.databases.readAll().fetchAll();
