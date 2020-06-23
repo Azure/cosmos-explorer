221aad85cde31941233a4429f6da32e5a89ee109
Steve Faulkner <stfaul@microsoft.com>
2020-06-23 10:10:04 -0500

    Remove lint exception

--- src/Explorer/Controls/NotebookGallery/Cards/GalleryCardComponent.tsx
+++ src/Explorer/Controls/NotebookGallery/Cards/GalleryCardComponent.tsx
@@ -22,9 +22,7 @@ export class GalleryCardComponent extends React.Component<GalleryCardComponentPr
   private attendantsCardSectionTokens: ICardSectionTokens = { childrenGap: 6 };
 
   public render(): JSX.Element {
-    // TODO: Remove this. Should not be using explicit null anywhere
-    // eslint-disable-next-line no-null/no-null
-    return this.props.notebookMetadata != null ? (
+    return this.props.notebookMetadata !== undefined ? (
       <Card aria-label="Notebook Card" onClick={this.props.onClick} tokens={this.cardTokens}>
         <Card.Item>
           <Persona text={this.props.notebookMetadata.author} secondaryText={this.props.notebookMetadata.date} />
