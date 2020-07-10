a824e4d5ea7bedf6d7659d80ad745283157c6441
Steve Faulkner <stfaul@microsoft.com>
2020-07-10 12:26:39 -0500

    Remove correlation iframe

--- src/Index.ts
+++ src/Index.ts
@@ -2,22 +2,12 @@ import "../less/index.less";
 import "./Libs/jquery";
 
 import * as ko from "knockout";
-import { CorrelationBackend } from "./Common/Constants";
-import Ajax from "./Shared/Ajax";
 
 class Index {
   public navigationSelection: ko.Observable<string>;
-  public correlationSrc: ko.Observable<string>;
 
   constructor() {
     this.navigationSelection = ko.observable("quickstart");
-    this.correlationSrc = ko.observable("");
-
-    Ajax.get("/_explorer/installation_id.txt").then(result => {
-      // TODO: Detect correct URL for each environment automatically.
-      const url: string = `${CorrelationBackend.Url}?emulator_id=${result}`;
-      this.correlationSrc(url);
-    });
   }
 
   public quickstart_click() {
