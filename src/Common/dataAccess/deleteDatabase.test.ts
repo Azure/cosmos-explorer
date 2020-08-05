e5ddce55995816a1ad29bf12efcb5fce852ec1eb
Steve Faulkner <southpolesteve@gmail.com>
2020-08-05 15:27:25 -0500

    Fix tests

--- src/Common/dataAccess/deleteDatabase.test.ts
+++ src/Common/dataAccess/deleteDatabase.test.ts
@@ -1,11 +1,14 @@
 jest.mock("../../Utils/arm/request");
+jest.mock("../MessageHandler");
 import { deleteDatabase } from "./deleteDatabase";
 import { armRequest } from "../../Utils/arm/request";
 import { AuthType } from "../../AuthType";
+import { sendCachedDataMessage } from "../MessageHandler";
 
 describe("deleteDatabase", () => {
   it("should call ARM if logged in with AAD", async () => {
     window.authType = AuthType.AAD;
+    (sendCachedDataMessage as jest.Mock).mockResolvedValue(undefined);
     await deleteDatabase("database");
     expect(armRequest).toHaveBeenCalled();
   });
