38464e43ad4ef71b5cb6a667e86b99f32b60f9b4
Tanuj Mittal <tamitta@microsoft.com>
2020-08-11 00:57:48 -0700

    Fix build failure

--- src/Utils/Base64Utils.ts
+++ src/Utils/Base64Utils.ts
@@ -1,7 +1,7 @@
 export const utf8ToB64 = (utf8Str: string): string => {
   return btoa(
     encodeURIComponent(utf8Str).replace(/%([0-9A-F]{2})/g, (_, args) => {
-      return String.fromCharCode(parseInt(args[0], 16));
+      return String.fromCharCode(parseInt(args, 16));
     })
   );
 };
