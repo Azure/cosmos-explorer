455722c31638a74e104cf6b1ec455d3ca0458d3d
victor-meng <56978073+victor-meng@users.noreply.github.com>
2020-08-11 18:36:42 -0700

    Fix deleteDatabase and deleteCollection with ARM (#143)

--- src/Common/dataAccess/deleteDatabase.test.ts
+++ src/Common/dataAccess/deleteDatabase.test.ts
@@ -6,6 +6,7 @@ import { AuthType } from "../../AuthType";
 import { updateUserContext } from "../../UserContext";
 import { DatabaseAccount } from "../../Contracts/DataModels";
 import { sendCachedDataMessage } from "../MessageHandler";
+import { DefaultAccountExperienceType } from "../../DefaultAccountExperienceType";
 
 describe("deleteDatabase", () => {
   it("should call ARM if logged in with AAD", async () => {
@@ -13,7 +14,8 @@ describe("deleteDatabase", () => {
     updateUserContext({
       databaseAccount: {
         name: "test"
-      } as DatabaseAccount
+      } as DatabaseAccount,
+      defaultExperience: DefaultAccountExperienceType.DocumentDB
     });
     (sendCachedDataMessage as jest.Mock).mockResolvedValue(undefined);
     await deleteDatabase("database");
