b8499aedbc2e815b759ef224217d611434550e14
Tanuj Mittal <tamitta@microsoft.com>
2020-08-12 11:02:04 -0700

    Add workaround for ignoring failed /offers when using connection string

--- src/Common/DataAccessUtilityBase.ts
+++ src/Common/DataAccessUtilityBase.ts
@@ -473,6 +473,13 @@ export function readOffers(options: any): Q.Promise<DataModels.Offer[]> {
       .offers.readAll()
       .fetchAll()
       .then(response => response.resources)
+      .catch(error => {
+        // This should be removed when we can correctly identify if an account is serverless when connected using connection string too.
+        if (error.message.includes("Reading or replacing offers is not supported for serverless accounts")) {
+          return [];
+        }
+        throw error;
+      })
   );
 }
 
