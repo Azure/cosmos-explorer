290ca4aba51614248a53255352cdf651c7b77465
Zachary Foster <zfoster@users.noreply.github.com>
2020-09-23 18:29:04 -0400

    Commits add table entity pane changes without steve's changes (#227)

--- src/Explorer/Panes/Tables/AddTableEntityPane.ts
+++ src/Explorer/Panes/Tables/AddTableEntityPane.ts
@@ -69,6 +69,7 @@ export default class AddTableEntityPane extends TableEntityPane {
           );
           this.updateIsActionEnabled();
           super.open();
+          this.focusValueElement();
         });
     } else {
       this.displayedAttributes(
@@ -79,8 +80,12 @@ export default class AddTableEntityPane extends TableEntityPane {
       );
       this.updateIsActionEnabled();
       super.open();
+      this.focusValueElement();
     }
-    const focusElement = document.getElementById("closeAddEntityPane");
+  }
+
+  private focusValueElement() {
+    const focusElement = document.getElementById("addTableEntityValue");
     focusElement && focusElement.focus();
   }
 
