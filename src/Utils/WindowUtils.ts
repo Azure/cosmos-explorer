66f9b73e0cd8e983d05a50964bdc218174eb6a7d
Tanuj Mittal <tamitta@microsoft.com>
2020-09-25 14:01:22 -0700

    Do not fail when trying to find DE window with cross origin

--- src/Utils/WindowUtils.ts
+++ src/Utils/WindowUtils.ts
@@ -2,15 +2,21 @@ export const getDataExplorerWindow = (currentWindow: Window): Window | undefined
   // Start with the current window and traverse up the parent hierarchy to find a window
   // with `dataExplorerPlatform` property
   let dataExplorerWindow: Window | undefined = currentWindow;
-  // TODO: Need to `any` here since the window imports Explorer which can't be in strict mode yet
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  while (dataExplorerWindow && (dataExplorerWindow as any).dataExplorerPlatform === undefined) {
-    // If a window does not have a parent, its parent property is a reference to itself.
-    if (dataExplorerWindow.parent === dataExplorerWindow) {
-      dataExplorerWindow = undefined;
-    } else {
-      dataExplorerWindow = dataExplorerWindow.parent;
+
+  try {
+    // TODO: Need to `any` here since the window imports Explorer which can't be in strict mode yet
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    while (dataExplorerWindow && (dataExplorerWindow as any).dataExplorerPlatform == undefined) {
+      // If a window does not have a parent, its parent property is a reference to itself.
+      if (dataExplorerWindow.parent == dataExplorerWindow) {
+        dataExplorerWindow = undefined;
+      } else {
+        dataExplorerWindow = dataExplorerWindow.parent;
+      }
     }
+  } catch (error) {
+    // This can happen if we come across parent from a different origin
+    dataExplorerWindow = undefined;
   }
 
   return dataExplorerWindow;
