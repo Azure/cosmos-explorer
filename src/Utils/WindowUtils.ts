6ab45647c4786ee1f377e20582e1079882707c20
Tanuj Mittal <tamitta@microsoft.com>
2020-09-25 14:08:28 -0700

    Fix lint errors

--- src/Utils/WindowUtils.ts
+++ src/Utils/WindowUtils.ts
@@ -6,9 +6,9 @@ export const getDataExplorerWindow = (currentWindow: Window): Window | undefined
   try {
     // TODO: Need to `any` here since the window imports Explorer which can't be in strict mode yet
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    while (dataExplorerWindow && (dataExplorerWindow as any).dataExplorerPlatform == undefined) {
+    while (dataExplorerWindow && (dataExplorerWindow as any).dataExplorerPlatform === undefined) {
       // If a window does not have a parent, its parent property is a reference to itself.
-      if (dataExplorerWindow.parent == dataExplorerWindow) {
+      if (dataExplorerWindow.parent === dataExplorerWindow) {
         dataExplorerWindow = undefined;
       } else {
         dataExplorerWindow = dataExplorerWindow.parent;
