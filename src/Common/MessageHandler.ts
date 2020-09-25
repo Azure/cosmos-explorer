823cbc4229a7a5d9b31f9ca49a7fa2becd61b7b2
Tanuj Mittal <tamitta@microsoft.com>
2020-09-25 13:49:46 -0700

    Do not fail when trying to find DE window with cross origin

--- src/Common/MessageHandler.ts
+++ src/Common/MessageHandler.ts
@@ -48,16 +48,15 @@ export function sendCachedDataMessage<TResponseDataModel>(
 
 export function sendMessage(data: any): void {
   if (canSendMessage()) {
-    const dataExplorerWindow = getDataExplorerWindow(window);
-    if (dataExplorerWindow) {
-      dataExplorerWindow.parent.postMessage(
-        {
-          signature: "pcIframe",
-          data: data
-        },
-        dataExplorerWindow.document.referrer
-      );
-    }
+    // We try to find data explorer window first, then fallback to current window
+    const portalChildWindow = getDataExplorerWindow(window) || window;
+    portalChildWindow.parent.postMessage(
+      {
+        signature: "pcIframe",
+        data: data
+      },
+      portalChildWindow.document.referrer
+    );
   }
 }
 
@@ -66,15 +65,21 @@ export const getDataExplorerWindow = (currentWindow: Window): Window | undefined
   // Start with the current window and traverse up the parent hierarchy to find a window
   // with `dataExplorerPlatform` property
   let dataExplorerWindow: Window | undefined = currentWindow;
-  // TODO: Need to `any` here since the window imports Explorer which can't be in strict mode yet
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  while (dataExplorerWindow && (dataExplorerWindow as any).dataExplorerPlatform == undefined) {
-    // If a window does not have a parent, its parent property is a reference to itself.
-    if (dataExplorerWindow.parent == dataExplorerWindow) {
-      dataExplorerWindow = undefined;
-    } else {
-      dataExplorerWindow = dataExplorerWindow.parent;
+
+  try {
+    // TODO: Need to `any` here since the window imports Explorer which can't be in strict mode yet
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    while (dataExplorerWindow && (dataExplorerWindow as any).dataExplorerPlatform == undefined) {
+      // If a window does not have a parent, its parent property is a reference to itself.
+      if (dataExplorerWindow.parent == dataExplorerWindow) {
+        dataExplorerWindow = undefined;
+      } else {
+        dataExplorerWindow = dataExplorerWindow.parent;
+      }
     }
+  } catch (error) {
+    // This can happen if we come across parent from a different origin
+    dataExplorerWindow = undefined;
   }
 
   return dataExplorerWindow;
