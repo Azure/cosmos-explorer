401ee7af2cb61c1f012cfe21d637f416e9ce843d
Steve Faulkner <471400+southpolesteve@users.noreply.github.com>
2020-09-29 17:43:20 -0500

    Handle more cases

--- src/ConfigContext.ts
+++ src/ConfigContext.ts
@@ -31,10 +31,11 @@ interface ConfigContext {
 let configContext: Readonly<ConfigContext> = {
   platform: Platform.Portal,
   allowedParentFrameOrigins: [
-    `^https:\\/\\/cosmos.azure.(com|cn|us)$`,
-    `^https:\\/\\/[\\.\\w]*portal.azure.(com|cn|us)$`,
-    `^https:\\/\\/[\\.\\w]*ext.azure.(com|cn|us)$`,
-    `^https:\\/\\/[\\.\\w]*microsoftazure.de$`
+    `^https:\\/\\/cosmos\\.azure\\.(com|cn|us)$`,
+    `^https:\\/\\/[\\.\\w]*portal\\.azure\\.(com|cn|us)$`,
+    `^https:\\/\\/[\\.\\w]*ext\\.azure\\.(com|cn|us)$`,
+    `^https:\\/\\/[\\.\\w]*\\.ext\\.microsoftazure\\.de$`,
+    `^https://cosmos-db-dataexplorer-germanycentral.azurewebsites.de$`
   ],
   // Webpack injects this at build time
   gitSha: process.env.GIT_SHA,
--- src/Utils/MessageValidation.test.ts
+++ src/Utils/MessageValidation.test.ts
@@ -5,14 +5,12 @@ test.each`
   ${"https://cosmos.azure.com"}                                    | ${false}
   ${"https://cosmos.azure.us"}                                     | ${false}
   ${"https://cosmos.azure.cn"}                                     | ${false}
-  ${"https://cosmos.microsoftazure.de"}                            | ${false}
   ${"https://portal.azure.com"}                                    | ${false}
   ${"https://portal.azure.us"}                                     | ${false}
   ${"https://portal.azure.cn"}                                     | ${false}
   ${"https://subdomain.portal.azure.com"}                          | ${false}
   ${"https://subdomain.portal.azure.us"}                           | ${false}
   ${"https://subdomain.portal.azure.cn"}                           | ${false}
-  ${"https://subdomain.microsoftazure.de"}                         | ${false}
   ${"https://main.documentdb.ext.azure.com"}                       | ${false}
   ${"https://main.documentdb.ext.azure.us"}                        | ${false}
   ${"https://main.documentdb.ext.azure.cn"}                        | ${false}
@@ -20,6 +18,8 @@ test.each`
   ${"https://random.domain"}                                       | ${true}
   ${"https://malicious.cloudapp.azure.com"}                        | ${true}
   ${"https://malicious.germanycentral.cloudapp.microsoftazure.de"} | ${true}
+  ${"https://maliciousazure.com"}                                  | ${true}
+  ${"https://maliciousportalsazure.com"}                           | ${true}
 `("returns $expected when called with $domain", ({ domain, expected }) => {
   expect(isInvalidParentFrameOrigin({ origin: domain } as MessageEvent)).toBe(expected);
 });
