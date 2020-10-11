2d39d2061de0e7a618f2440129bd911bceda5408
artrejo <ato9000@users.noreply.github.com>
2020-10-11 03:02:19 -0700

    Null is not allowed by lint rules

--- src/Explorer/Tables/CqlUtilities.test.ts
+++ src/Explorer/Tables/CqlUtilities.test.ts
@@ -1,11 +1,6 @@
 import { getQuotedCqlIdentifier } from "./CqlUtilities";
 
 describe("getQuotedCqlIdentifier", () => {
-  it("null id", () => {
-    const result = getQuotedCqlIdentifier(null);
-    expect(result).toBe(null);
-  });
-
   it("undefined id", () => {
     const result = getQuotedCqlIdentifier(undefined);
     expect(result).toBe(undefined);
