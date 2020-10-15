42356a8eb1306098698d628fc2d8f5ee3e102ee6
Victor Meng <vimeng@microsoft.com>
2020-10-14 17:18:10 -0700

    Move stored procedure operations to RP

--- src/Common/dataAccess/updateStoredProcedure.ts
+++ src/Common/dataAccess/updateStoredProcedure.ts
@@ -1,29 +1,70 @@
+import { AuthType } from "../../AuthType";
 import { Resource, StoredProcedureDefinition } from "@azure/cosmos";
-import { logConsoleError, logConsoleProgress } from "../../Utils/NotificationConsoleUtils";
+import {
+  SqlStoredProcedureCreateUpdateParameters,
+  SqlStoredProcedureResource
+} from "../../Utils/arm/generatedClients/2020-04-01/types";
 import { client } from "../CosmosClient";
+import { logConsoleError, logConsoleProgress } from "../../Utils/NotificationConsoleUtils";
+import {
+  createUpdateSqlStoredProcedure,
+  getSqlStoredProcedure
+} from "../../Utils/arm/generatedClients/2020-04-01/sqlResources";
 import { logError } from "../Logger";
 import { sendNotificationForError } from "./sendNotificationForError";
+import { userContext } from "../../UserContext";
 
 export async function updateStoredProcedure(
   databaseId: string,
   collectionId: string,
   storedProcedure: StoredProcedureDefinition
 ): Promise<StoredProcedureDefinition & Resource> {
-  let updatedStoredProcedure: StoredProcedureDefinition & Resource;
   const clearMessage = logConsoleProgress(`Updating stored procedure ${storedProcedure.id}`);
   try {
+    if (window.authType === AuthType.AAD && !userContext.useSDKOperations) {
+      const getResponse = await getSqlStoredProcedure(
+        userContext.subscriptionId,
+        userContext.resourceGroup,
+        userContext.databaseAccount.name,
+        databaseId,
+        collectionId,
+        storedProcedure.id
+      );
+
+      if (getResponse?.properties?.resource) {
+        const createSprocParams: SqlStoredProcedureCreateUpdateParameters = {
+          properties: {
+            resource: storedProcedure as SqlStoredProcedureResource,
+            options: {}
+          }
+        };
+        const rpResponse = await createUpdateSqlStoredProcedure(
+          userContext.subscriptionId,
+          userContext.resourceGroup,
+          userContext.databaseAccount.name,
+          databaseId,
+          collectionId,
+          storedProcedure.id,
+          createSprocParams
+        );
+        return rpResponse && (rpResponse.properties?.resource as StoredProcedureDefinition & Resource);
+      }
+
+      throw new Error(`Failed to update stored procedure: ${storedProcedure.id} does not exist.`);
+    }
+
     const response = await client()
       .database(databaseId)
       .container(collectionId)
       .scripts.storedProcedure(storedProcedure.id)
       .replace(storedProcedure);
-    updatedStoredProcedure = response.resource;
+    return response?.resource;
   } catch (error) {
     logConsoleError(`Error while updating stored procedure ${storedProcedure.id}:\n ${JSON.stringify(error)}`);
     logError(JSON.stringify(error), "UpdateStoredProcedure", error.code);
     sendNotificationForError(error);
+    throw error;
+  } finally {
+    clearMessage();
   }
-
-  clearMessage();
-  return updatedStoredProcedure;
 }
