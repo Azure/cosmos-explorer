11db704c0d389f568938e25591361727f039fb99
Steve Faulkner <471400+southpolesteve@users.noreply.github.com>
2020-10-16 14:29:45 -0500

    Fix getDataExplorerWindow

--- src/Utils/WindowUtils.ts
+++ src/Utils/WindowUtils.ts
@@ -1,23 +1,18 @@
 export const getDataExplorerWindow = (currentWindow: Window): Window | undefined => {
-  // Start with the current window and traverse up the parent hierarchy to find a window
-  // with `dataExplorerPlatform` property
-  let dataExplorerWindow: Window | undefined = currentWindow;
-
+  // Data explorer is always loaded in an iframe, so traverse the parents until you hit the top and return the first child window.
   try {
-    // TODO: Need to `any` here since the window imports Explorer which can't be in strict mode yet
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    while (dataExplorerWindow && (dataExplorerWindow as any).dataExplorerPlatform === undefined) {
-      // If a window does not have a parent, its parent property is a reference to itself.
-      if (dataExplorerWindow.parent === dataExplorerWindow) {
-        dataExplorerWindow = undefined;
-      } else {
-        dataExplorerWindow = dataExplorerWindow.parent;
+    while (currentWindow) {
+      if (currentWindow.parent === currentWindow) {
+        return undefined;
       }
+      if (currentWindow.parent === currentWindow.top) {
+        return currentWindow;
+      }
+      currentWindow = currentWindow.parent;
     }
   } catch (error) {
-    // This can happen if we come across parent from a different origin
-    dataExplorerWindow = undefined;
+    // Hitting a cross domain error means we are in the portal and the current window is data explorer
+    return currentWindow;
   }
-
-  return dataExplorerWindow;
+  return undefined;
 };
