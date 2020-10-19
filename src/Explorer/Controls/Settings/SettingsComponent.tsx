4f8794af4bd070f90cff7f582a4a104b5d7cd60a
Srinath Narayanan <srnara@microsoft.com>
2020-10-19 16:52:15 -0700

    minor edits

--- src/Explorer/Controls/Settings/SettingsComponent.tsx
+++ src/Explorer/Controls/Settings/SettingsComponent.tsx
@@ -473,8 +473,8 @@ export class SettingsComponent extends React.Component<SettingsComponentProps, S
         Action.SettingsV2Updated,
         {
           databaseAccountName: this.container.databaseAccount()?.name,
-          databaseName: this.collection && this.collection.databaseId,
-          collectionName: this.collection && this.collection.id(),
+          databaseName: this.collection?.databaseId,
+          collectionName: this.collection?.id(),
           defaultExperience: this.container.defaultExperience(),
           dataExplorerArea: Constants.Areas.Tab,
           tabTitle: this.props.settingsTab.tabTitle(),
