365a252b8da0142600cbe0b2b40da163e8fcc987
Steve Faulkner <471400+southpolesteve@users.noreply.github.com>
2021-01-08 11:56:34 -0600

    Remove console.log

--- src/Explorer/Tabs/QueryTab.ts
+++ src/Explorer/Tabs/QueryTab.ts
@@ -121,7 +121,6 @@ export default class QueryTab extends TabsBase implements ViewModels.WaitsForTem
 
     this.maybeSubQuery = ko.computed<boolean>(function() {
       const sql = this.sqlQueryEditorContent();
-      console.log(sql, /.*\(.*SELECT.*\)/i.test(sql));
       return sql && /.*\(.*SELECT.*\)/i.test(sql);
     }, this);
 
