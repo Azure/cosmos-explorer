a678b88f531ad7c8a9d6c982918d02c838ae205d
Steve Faulkner <471400+southpolesteve@users.noreply.github.com>
2021-01-21 09:54:39 -0600

    Fix up lint

--- utils/codeMetrics.js
+++ utils/codeMetrics.js
@@ -1,13 +1,30 @@
+/* eslint-disable no-console */
+const fs = require("fs");
 const fg = require("fast-glob");
 const appInsights = require("applicationinsights");
 appInsights.setup("a19ced82-51c3-4fce-9d26-3b13a77a70be").start();
 
 const client = appInsights.defaultClient;
 const htmlFiles = fg.sync(["**/*.html", "!node_modules"]);
+const strictModeJSON = require("../tsconfig.strict.json");
+const eslintIgnore = fs.readFileSync(".eslintignore", { encoding: "utf8" });
 
+console.log("HTML File Count", htmlFiles.length);
 client.trackMetric({
   name: "HTML File Count",
   value: htmlFiles.length,
+});
+
+console.log("TypeScript Strict File Count", strictModeJSON.files.length);
+client.trackMetric({
+  name: "TypeScript Strict File Count",
+  value: strictModeJSON.files.length,
+});
+
+console.log("Unlinted File Count", eslintIgnore.split("\n").length);
+client.trackMetric({
+  name: "Unlinted File Count",
+  value: eslintIgnore.split("\n").length,
 });
 
 appInsights.defaultClient.flush({
