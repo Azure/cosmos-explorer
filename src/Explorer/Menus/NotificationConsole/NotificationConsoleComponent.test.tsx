06538c532aae017c2447ade070b20385263c1da7
Victor Meng <vimeng@microsoft.com>
2021-01-25 17:17:21 -0800

    Remove unnecessary changes

--- src/Explorer/Menus/NotificationConsole/NotificationConsoleComponent.test.tsx
+++ src/Explorer/Menus/NotificationConsole/NotificationConsoleComponent.test.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import { shallow } from "enzyme";
 import {
   NotificationConsoleComponentProps,
-  ConsoleData,
   NotificationConsoleComponent,
   ConsoleDataType,
 } from "./NotificationConsoleComponent";
