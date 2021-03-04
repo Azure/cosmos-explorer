20b60d686f1f576b1e254832b62c5d5cc79eb330
sunilyadav840 <sy00733526@TechMahindra.com>
2021-03-04 13:52:59 +0530

    fixed lint issue of DateTimeUtility

--- src/Explorer/Tables/QueryBuilder/DateTimeUtilities.ts
+++ src/Explorer/Tables/QueryBuilder/DateTimeUtilities.ts
@@ -2,26 +2,26 @@ const epochTicks = 621355968000000000;
 const ticksPerMillisecond = 10000;
 
 export function getLocalDateTime(dateTime: string): string {
-  var dateTimeObject: Date = new Date(dateTime);
-  var year: number = dateTimeObject.getFullYear();
-  var month: string = ensureDoubleDigits(dateTimeObject.getMonth() + 1); // Month ranges from 0 to 11
-  var day: string = ensureDoubleDigits(dateTimeObject.getDate());
-  var hours: string = ensureDoubleDigits(dateTimeObject.getHours());
-  var minutes: string = ensureDoubleDigits(dateTimeObject.getMinutes());
-  var seconds: string = ensureDoubleDigits(dateTimeObject.getSeconds());
-  var milliseconds: string = ensureTripleDigits(dateTimeObject.getMilliseconds());
+  const dateTimeObject: Date = new Date(dateTime);
+  const year: number = dateTimeObject.getFullYear();
+  const month: string = ensureDoubleDigits(dateTimeObject.getMonth() + 1); // Month ranges from 0 to 11
+  const day: string = ensureDoubleDigits(dateTimeObject.getDate());
+  const hours: string = ensureDoubleDigits(dateTimeObject.getHours());
+  const minutes: string = ensureDoubleDigits(dateTimeObject.getMinutes());
+  const seconds: string = ensureDoubleDigits(dateTimeObject.getSeconds());
+  const milliseconds: string = ensureTripleDigits(dateTimeObject.getMilliseconds());
 
-  var localDateTime: string = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}`;
+  const localDateTime = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}`;
   return localDateTime;
 }
 
 export function getUTCDateTime(dateTime: string): string {
-  var dateTimeObject: Date = new Date(dateTime);
+  const dateTimeObject: Date = new Date(dateTime);
   return dateTimeObject.toISOString();
 }
 
 export function ensureDoubleDigits(num: number): string {
-  var doubleDigitsString: string = num.toString();
+  let doubleDigitsString: string = num.toString();
   if (num < 10) {
     doubleDigitsString = `0${doubleDigitsString}`;
   } else if (num > 99) {
@@ -31,7 +31,7 @@ export function ensureDoubleDigits(num: number): string {
 }
 
 export function ensureTripleDigits(num: number): string {
-  var tripleDigitsString: string = num.toString();
+  let tripleDigitsString: string = num.toString();
   if (num < 10) {
     tripleDigitsString = `00${tripleDigitsString}`;
   } else if (num < 100) {
@@ -51,17 +51,17 @@ export function convertJSDateToUnix(dateTime: string): number {
 }
 
 export function convertTicksToJSDate(ticks: string): Date {
-  var ticksJSBased = Number(ticks) - epochTicks;
-  var timeInMillisecond = ticksJSBased / ticksPerMillisecond;
+  const ticksJSBased = Number(ticks) - epochTicks;
+  const timeInMillisecond = ticksJSBased / ticksPerMillisecond;
   return new Date(timeInMillisecond);
 }
 
 export function convertJSDateToTicksWithPadding(dateTime: string): string {
-  var ticks = epochTicks + new Date(dateTime).getTime() * ticksPerMillisecond;
+  const ticks = epochTicks + new Date(dateTime).getTime() * ticksPerMillisecond;
   return padDateTicksWithZeros(ticks.toString());
 }
 
 function padDateTicksWithZeros(value: string): string {
-  var s = "0000000000000000000" + value;
+  const s = "0000000000000000000" + value;
   return s.substr(s.length - 20);
 }
