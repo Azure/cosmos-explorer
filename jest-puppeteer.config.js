c48f3cf998efa129ff1c19d353ff6c6a81fa7abe
Steve Faulkner <stfaul@microsoft.com>
2021-03-05 11:10:59 -0600

    Speed up sloMo

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -3,7 +3,7 @@ const isCI = require("is-ci");
 module.exports = {
   launch: {
     headless: isCI,
-    slowMo: 55,
+    slowMo: 10,
     defaultViewport: null,
     ignoreHTTPSErrors: true,
     args: ["--disable-web-security"],
