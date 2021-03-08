f59e2892d66f1b4f8cef7c2e010ce63888970eda
sunilyadav840 <sy00733526@techmahindra.com>
2021-03-08 18:27:49 +0530

    Fixed import error

--- src/Explorer/Notebook/NTeractUtil.ts
+++ src/Explorer/Notebook/NTeractUtil.ts
@@ -3,18 +3,18 @@ import { NotebookContentRecordProps, selectors } from "@nteract/core";
 /**
  * A bunch of utilities to interact with nteract
  */
-  export function getCurrentCellType(content: NotebookContentRecordProps): "markdown" | "code" | "raw" | undefined {
-    if (!content) {
-      return undefined;
-    }
-
-    const cellFocusedId = selectors.notebook.cellFocused(content.model);
-    if (cellFocusedId) {
-      const cell = selectors.notebook.cellById(content.model, { id: cellFocusedId });
-      if (cell) {
-        return cell.cell_type;
-      }
-    }
-
+export function getCurrentCellType(content: NotebookContentRecordProps): "markdown" | "code" | "raw" | undefined {
+  if (!content) {
     return undefined;
   }
+
+  const cellFocusedId = selectors.notebook.cellFocused(content.model);
+  if (cellFocusedId) {
+    const cell = selectors.notebook.cellById(content.model, { id: cellFocusedId });
+    if (cell) {
+      return cell.cell_type;
+    }
+  }
+
+  return undefined;
+}
