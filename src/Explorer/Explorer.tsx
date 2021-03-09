a262ece07047208ff800a6b9dd7f07741790bf28
Jordi Bunster <jbunster@microsoft.com>
2021-03-08 16:29:26 -0800

    Explorer: allow for injecting TabsManager

--- src/Explorer/Explorer.tsx
+++ src/Explorer/Explorer.tsx
@@ -95,6 +95,7 @@ export interface ExplorerParams {
   closeSidePanel: () => void;
   closeDialog: () => void;
   openDialog: (props: DialogProps) => void;
+  tabsManager: TabsManager;
 }
 
 export default class Explorer {
@@ -622,7 +623,7 @@ export default class Explorer {
       container: this,
     });
 
-    this.tabsManager = new TabsManager();
+    this.tabsManager = params?.tabsManager ?? new TabsManager();
 
     this._panes = [
       this.addDatabasePane,
