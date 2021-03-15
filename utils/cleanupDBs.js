73c8cf771407ef8cf61fd00c1b8995db4e7f57c1
Steve Faulkner <southpolesteve@gmail.com>
2021-03-14 21:50:37 -0500

    Move to 30 minutes ago

--- utils/cleanupDBs.js
+++ utils/cleanupDBs.js
@@ -9,7 +9,7 @@ const tenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47";
 const subscriptionId = "69e02f2d-f059-4409-9eac-97e8a276ae2c";
 const resourceGroupName = "runners";
 
-const sixtyMinutesAgo = new Date(Date.now() - 1000 * 60 * 60).getTime();
+const thirtyMinutesAgo = new Date(Date.now() - 1000 * 60 * 30).getTime();
 
 function friendlyTime(date) {
   try {
@@ -29,7 +29,7 @@ async function main() {
       const mongoDatabases = await client.mongoDBResources.listMongoDBDatabases(resourceGroupName, account.name);
       for (const database of mongoDatabases) {
         const timestamp = Number(database.name.split("-")[1]);
-        if (timestamp && timestamp < sixtyMinutesAgo) {
+        if (timestamp && timestamp < thirtyMinutesAgo) {
           await client.mongoDBResources.deleteMongoDBDatabase(resourceGroupName, account.name, database.name);
           console.log(`DELETED: ${account.name} | ${database.name} | Age: ${friendlyTime(Date.now() - timestamp)}`);
         } else {
@@ -40,7 +40,7 @@ async function main() {
       const sqlDatabases = await client.sqlResources.listSqlDatabases(resourceGroupName, account.name);
       for (const database of sqlDatabases) {
         const timestamp = Number(database.name.split("-")[1]);
-        if (timestamp && timestamp < sixtyMinutesAgo) {
+        if (timestamp && timestamp < thirtyMinutesAgo) {
           await client.sqlResources.deleteSqlDatabase(resourceGroupName, account.name, database.name);
           console.log(`DELETED: ${account.name} | ${database.name} | Age: ${friendlyTime(Date.now() - timestamp)}`);
         } else {
