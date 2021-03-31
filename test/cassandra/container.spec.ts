074e16037e69d74a45f8349b55cea44d6d0b01ff
Steve Faulkner <stfaul@microsoft.com>
2021-03-31 17:17:36 -0500

    More fix

--- test/cassandra/container.spec.ts
+++ test/cassandra/container.spec.ts
@@ -107,9 +107,9 @@ describe("Collection Add and Delete Cassandra spec", () => {
       await dbElements[0].click();
 
       // confirm delete database
-      await frame.waitForSelector('input[data-test="confirmDatabaseId"]', { visible: true });
+      await frame.waitForSelector('input[id="confirmDatabaseId"]', { visible: true });
       await frame.waitFor(RENDER_DELAY);
-      await frame.type('input[data-test="confirmDatabaseId"]', keyspaceId.trim());
+      await frame.type('input[id="confirmDatabaseId"]', keyspaceId.trim());
 
       // click delete
       await frame.click('button[id="sidePanelOkButton"]');
