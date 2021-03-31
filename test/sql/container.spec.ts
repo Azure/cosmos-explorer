1c63bc97f9b3837e64c603390579c3abcb46afd0
Steve Faulkner <stfaul@microsoft.com>
2021-03-31 16:36:44 -0500

    More fix

--- test/sql/container.spec.ts
+++ test/sql/container.spec.ts
@@ -125,7 +125,7 @@ describe("Collection Add and Delete SQL spec", () => {
       await frame.type('input[id="confirmDatabaseId"]', selectedDbId);
 
       // click delete
-      await frame.click('input[id="sidePanelOkButton"]');
+      await frame.click('button[id="sidePanelOkButton"]');
       await frame.waitForSelector('div[class="splashScreen"] > div[class="title"]', { visible: true });
       await frame.waitFor(LOADING_STATE_DELAY);
       await frame.waitForSelector('div[class="splashScreen"] > div[class="title"]', { visible: true });
