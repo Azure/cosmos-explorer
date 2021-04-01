d6f4f757349086f9e4dbfa1fff0bfc333105bcd7
Jordi Bunster <jbunster@microsoft.com>
2021-04-01 15:41:51 -0700

    Switch to accessibility insights's version of these tools

---
+++ strict-null-checks/eligible-file-finder.js
@@ -0,0 +1,70 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// @ts-check
+const fs = require("fs");
+const path = require("path");
+const glob = require("glob");
+const config = require("./config");
+const { getMemoizedImportsForFile } = require("./import-finder");
+
+// "Eligible" means "a file that we might want to list in tsconfig.strictNullChecks.json"
+// "Checked" means "a file that is currently listed in tsconfig.strictNullChecks.json"
+// It is possible for an ineligible file to be checked (eg, a png under /src/icons/**)
+
+const isEligibleFile = (file) => !config.skippedFiles.has(path.relative(config.srcRoot, file));
+
+function globAsync(pattern) {
+  return new Promise((resolve, reject) => glob(pattern, (err, files) => (err ? reject(err) : resolve(files))));
+}
+
+// Includes both checked and unchecked files (ie, doesn't care about inclusion in tsconfig.strictNullChecks.json)
+async function getAllEligibleFiles() {
+  const tsFiles = await globAsync(`${config.srcRoot}/**/*.@(ts|tsx)`);
+  return tsFiles.filter(isEligibleFile);
+}
+
+// Includes ineligible files that are listed under glob patterns in tsconfig.strictNullChecks
+async function getAllCheckedFiles() {
+  const tsconfigPath = path.join(config.repoRoot, config.targetTsconfig);
+  const tsconfigContent = JSON.parse(fs.readFileSync(tsconfigPath).toString());
+
+  const set = new Set(tsconfigContent.files.map((f) => path.join(config.repoRoot, f).replace(/\\/g, "/")));
+  await Promise.all(
+    tsconfigContent.include.map(async (include) => {
+      const includePath = path.join(config.repoRoot, include);
+      const files = await globAsync(includePath);
+      for (const file of files) {
+        set.add(file);
+      }
+    })
+  );
+  return set;
+}
+
+async function getUncheckedLeafFiles() {
+  const checkedFiles = await getAllCheckedFiles();
+  const eligibleFiles = await getAllEligibleFiles();
+  const eligibleFileSet = new Set(eligibleFiles);
+  const allUncheckedFiles = eligibleFiles.filter((file) => !checkedFiles.has(file));
+
+  const areAllImportsChecked = (file) => {
+    const allImports = getMemoizedImportsForFile(file, config.srcRoot);
+    const uncheckedImports = allImports.filter((imp) => !checkedFiles.has(imp));
+    const ineligibleUncheckedImports = uncheckedImports.filter((imp) => !eligibleFileSet.has(imp));
+    if (ineligibleUncheckedImports.length > 0) {
+      console.warn(
+        `Eligible file ${file} with unchecked ineligible imports [${ineligibleUncheckedImports.join(", ")}]`
+      );
+    }
+    return uncheckedImports.length === 0;
+  };
+
+  return allUncheckedFiles.filter(areAllImportsChecked);
+}
+
+module.exports = {
+  getAllEligibleFiles,
+  getUncheckedLeafFiles,
+  getAllCheckedFiles,
+};
---
+++ strict-null-checks/find.js
@@ -0,0 +1,63 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// @ts-check
+const path = require("path");
+const process = require("process");
+const { srcRoot } = require("./config");
+const { getUncheckedLeafFiles, getAllEligibleFiles } = require("./eligible-file-finder");
+const { getImportsForFile } = require("./import-finder");
+
+if (process.argv.includes("--help")) {
+  console.log("yarn null:find [--sort=name|count] [--show-count] [--filter file_path_substring]");
+  process.exit(0);
+}
+const sortBy = process.argv.includes("--sort=name") ? "name" : "count";
+const printDependedOnCount = process.argv.includes("--show-count");
+const filterArgIndex = process.argv.indexOf("--filter") + 1;
+const filterArg = filterArgIndex === 0 ? null : process.argv[filterArgIndex];
+const filter = filterArg && ((file) => file.includes(filterArg));
+
+async function main() {
+  const eligibleFiles = await getUncheckedLeafFiles();
+
+  const eligibleSet = new Set(eligibleFiles);
+
+  const dependedOnCount = new Map(eligibleFiles.map((file) => [file, 0]));
+
+  for (const file of await getAllEligibleFiles()) {
+    if (eligibleSet.has(file)) {
+      // Already added
+      continue;
+    }
+
+    for (const imp of getImportsForFile(file, srcRoot)) {
+      if (dependedOnCount.has(imp)) {
+        dependedOnCount.set(imp, dependedOnCount.get(imp) + 1);
+      }
+    }
+  }
+
+  let out = Array.from(dependedOnCount.entries());
+  if (filter) {
+    out = out.filter((x) => filter(x[0]));
+  }
+  if (sortBy === "count") {
+    out = out.sort((a, b) => b[1] - a[1]);
+  } else if (sortBy === "name") {
+    out = out.sort((a, b) => a[0].localeCompare(b[0]));
+  }
+  for (const pair of out) {
+    console.log(toFormattedFilePath(pair[0]) + (printDependedOnCount ? ` — Depended on by **${pair[1]}** files` : ""));
+  }
+}
+
+function toFormattedFilePath(file) {
+  const relativePath = path.relative(srcRoot, file).replace(/\\/g, "/");
+  return `"./src/${relativePath}",`;
+}
+
+main().catch((error) => {
+  console.error(error.stack);
+  process.exit(1);
+});
