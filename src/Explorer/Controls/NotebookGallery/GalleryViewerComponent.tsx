bd8e78a81be8bb269ca1c2f34996c62814625daa
hardiknai-techm <HN00734461@TechMahindra.com>
2021-04-09 10:42:29 +0530

    migrate Gallery card and viewer component

--- src/Explorer/Controls/NotebookGallery/GalleryViewerComponent.tsx
+++ src/Explorer/Controls/NotebookGallery/GalleryViewerComponent.tsx
@@ -21,18 +21,18 @@ import {
   Text,
 } from "office-ui-fabric-react";
 import * as React from "react";
+import { HttpStatusCodes } from "../../../Common/Constants";
+import { handleError } from "../../../Common/ErrorHandlingUtils";
 import { IGalleryItem, IJunoResponse, IPublicGalleryData, JunoClient } from "../../../Juno/JunoClient";
+import { Action, ActionModifiers } from "../../../Shared/Telemetry/TelemetryConstants";
+import { trace } from "../../../Shared/Telemetry/TelemetryProcessor";
 import * as GalleryUtils from "../../../Utils/GalleryUtils";
+import Explorer from "../../Explorer";
 import { Dialog, DialogProps } from "../Dialog";
 import { GalleryCardComponent, GalleryCardComponentProps } from "./Cards/GalleryCardComponent";
-import "./GalleryViewerComponent.less";
-import { HttpStatusCodes } from "../../../Common/Constants";
-import Explorer from "../../Explorer";
 import { CodeOfConductComponent } from "./CodeOfConductComponent";
+import "./GalleryViewerComponent.less";
 import { InfoComponent } from "./InfoComponent/InfoComponent";
-import { handleError } from "../../../Common/ErrorHandlingUtils";
-import { trace } from "../../../Shared/Telemetry/TelemetryProcessor";
-import { Action, ActionModifiers } from "../../../Shared/Telemetry/TelemetryConstants";
 
 export interface GalleryViewerComponentProps {
   container?: Explorer;
