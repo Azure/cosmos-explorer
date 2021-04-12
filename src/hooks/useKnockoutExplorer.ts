662c03580ad76f6546ced2ba0870840024d876a1
Steve Faulkner <southpolesteve@gmail.com>
2021-04-12 11:28:52 -0500

    Call readCollections for Mongo using ARM (#636)

--- src/hooks/useKnockoutExplorer.ts
+++ src/hooks/useKnockoutExplorer.ts
@@ -201,11 +201,12 @@ async function configurePortal(explorerParams: ExplorerParams): Promise<Explorer
     if (process.env.NODE_ENV === "development" && !window.location.search.includes("disablePortalInitCache")) {
       const initMessage = sessionStorage.getItem("portalDataExplorerInitMessage");
       if (initMessage) {
-        const message = JSON.parse(initMessage);
+        const message = JSON.parse(initMessage) as DataExplorerInputsFrame;
         console.warn(
           "Loaded cached portal iframe message from session storage. Do a full page refresh to get a new message"
         );
         console.dir(message);
+        updateContextsFromPortalMessage(message);
         const explorer = new Explorer(explorerParams);
         explorer.configure(message);
         resolve(explorer);
@@ -237,29 +238,7 @@ async function configurePortal(explorerParams: ExplorerParams): Promise<Explorer
             inputs.extensionEndpoint = configContext.PROXY_PATH;
           }
 
-          const authorizationToken = inputs.authorizationToken || "";
-          const masterKey = inputs.masterKey || "";
-          const databaseAccount = inputs.databaseAccount;
-
-          updateConfigContext({
-            BACKEND_ENDPOINT: inputs.extensionEndpoint || configContext.BACKEND_ENDPOINT,
-            ARM_ENDPOINT: normalizeArmEndpoint(inputs.csmEndpoint || configContext.ARM_ENDPOINT),
-          });
-
-          updateUserContext({
-            authorizationToken,
-            masterKey,
-            databaseAccount,
-            resourceGroup: inputs.resourceGroup,
-            subscriptionId: inputs.subscriptionId,
-            subscriptionType: inputs.subscriptionType,
-            quotaId: inputs.quotaId,
-            portalEnv: inputs.serverId as PortalEnv,
-            hasWriteAccess: inputs.hasWriteAccess ?? true,
-            addCollectionFlight:
-              inputs.addCollectionDefaultFlight || CollectionCreation.DefaultAddCollectionDefaultFlight,
-          });
-
+          updateContextsFromPortalMessage(inputs);
           const explorer = new Explorer(explorerParams);
           explorer.configure(inputs);
           resolve(explorer);
@@ -297,6 +276,38 @@ function shouldProcessMessage(event: MessageEvent): boolean {
   }
 
   return true;
+}
+
+function updateContextsFromPortalMessage(inputs: DataExplorerInputsFrame) {
+  if (
+    configContext.BACKEND_ENDPOINT &&
+    configContext.platform === Platform.Portal &&
+    process.env.NODE_ENV === "development"
+  ) {
+    inputs.extensionEndpoint = configContext.PROXY_PATH;
+  }
+
+  const authorizationToken = inputs.authorizationToken || "";
+  const masterKey = inputs.masterKey || "";
+  const databaseAccount = inputs.databaseAccount;
+
+  updateConfigContext({
+    BACKEND_ENDPOINT: inputs.extensionEndpoint || configContext.BACKEND_ENDPOINT,
+    ARM_ENDPOINT: normalizeArmEndpoint(inputs.csmEndpoint || configContext.ARM_ENDPOINT),
+  });
+
+  updateUserContext({
+    authorizationToken,
+    masterKey,
+    databaseAccount,
+    resourceGroup: inputs.resourceGroup,
+    subscriptionId: inputs.subscriptionId,
+    subscriptionType: inputs.subscriptionType,
+    quotaId: inputs.quotaId,
+    portalEnv: inputs.serverId as PortalEnv,
+    hasWriteAccess: inputs.hasWriteAccess ?? true,
+    addCollectionFlight: inputs.addCollectionDefaultFlight || CollectionCreation.DefaultAddCollectionDefaultFlight,
+  });
 }
 
 interface PortalMessage {
