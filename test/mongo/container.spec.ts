f00f6351d7ea83d69dcd6ae150027c4a794a06ed
Steve Faulkner <southpolesteve@gmail.com>
2021-04-19 15:15:38 -0500

    Add indexing policy to mongo test

--- test/mongo/container.spec.ts
+++ test/mongo/container.spec.ts
@@ -2,7 +2,7 @@ import { jest } from "@jest/globals";
 import "expect-playwright";
 import { safeClick } from "../utils/safeClick";
 import { generateUniqueName } from "../utils/shared";
-jest.setTimeout(120000);
+jest.setTimeout(240000);
 
 test("SQL CRUD", async () => {
   const databaseId = generateUniqueName("db");
@@ -14,6 +14,7 @@ test("SQL CRUD", async () => {
     name: "explorer",
   });
 
+  // Create new database and collection
   await explorer.click('[data-test="New Collection"]');
   await explorer.click('[data-test="addCollection-newDatabaseId"]');
   await explorer.fill('[data-test="addCollection-newDatabaseId"]', databaseId);
@@ -25,6 +26,19 @@ test("SQL CRUD", async () => {
   await explorer.fill('[data-test="addCollection-partitionKeyValue"]', "/pk");
   await explorer.click('[data-test="addCollection-createCollection"]');
   await safeClick(explorer, `.nodeItem >> text=${databaseId}`);
+  await safeClick(explorer, `.nodeItem >> text=${containerId}`);
+  // Create indexing policy
+  await safeClick(explorer, ".nodeItem >> text=Settings");
+  await explorer.click('button[role="tab"]:has-text("Indexing Policy")');
+  await explorer.click('[aria-label="Index Field Name 0"]');
+  await explorer.fill('[aria-label="Index Field Name 0"]', "foo");
+  await explorer.click("text=Select an index type");
+  await explorer.click('button[role="option"]:has-text("Single Field")');
+  await explorer.click('[data-test="Save"]');
+  // Remove indexing policy
+  await explorer.click('[aria-label="Delete index Button"]');
+  await explorer.click('[data-test="Save"]');
+  // Delete database and collection
   await safeClick(explorer, `[data-test="${containerId}"] [aria-label="More"]`);
   await safeClick(explorer, 'button[role="menuitem"]:has-text("Delete Collection")');
   await explorer.fill('text=* Confirm by typing the collection id >> input[type="text"]', containerId);
