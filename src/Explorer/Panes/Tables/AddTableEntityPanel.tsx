729e491ac7864ab6a422af449493643eddcb0a87
sunilyadav840 <sy00733526@techmahindra.com>
2021-04-21 16:03:00 +0530

    Fixed PR change request

--- src/Explorer/Panes/Tables/AddTableEntityPanel.tsx
+++ src/Explorer/Panes/Tables/AddTableEntityPanel.tsx
@@ -83,6 +83,10 @@ export const AddTableEntityPanel: FunctionComponent<AddTableEntityPanelProps> = 
 
   /* Get default and previous saved entity headers */
   useEffect(() => {
+    getDefaultEntitiesAttribute();
+  }, []);
+
+  const getDefaultEntitiesAttribute = async (): Promise<void> => {
     let headers = tableEntityListViewModel.headers;
     if (DataTableUtilities.checkForDefaultHeader(headers)) {
       headers = [];
@@ -91,37 +95,35 @@ export const AddTableEntityPanel: FunctionComponent<AddTableEntityPanelProps> = 
       }
     }
     if (userContext.apiType === "Cassandra") {
-      cassandraApiClient.getTableSchema(queryTablesTab.collection).then((columns: CassandraTableKey[]) => {
-        const cassandraEntities = Utilities.getDataTypesFromCassandraSchema(columns);
-        const cassandraDefaultEntities: EntityRowType[] = getCassandraDefaultEntities(headers, cassandraEntities);
-        setEntities(cassandraDefaultEntities);
-      });
+      const columns: CassandraTableKey[] = await cassandraApiClient.getTableSchema(queryTablesTab.collection);
+      const cassandraEntities = Utilities.getDataTypesFromCassandraSchema(columns);
+      const cassandraDefaultEntities: EntityRowType[] = getCassandraDefaultEntities(headers, cassandraEntities);
+      setEntities(cassandraDefaultEntities);
     } else {
       const entityItems = tableEntityListViewModel.items();
       const entityTypes = Utilities.getDataTypesFromEntities(headers, entityItems);
       const defaultEntities: EntityRowType[] = getDefaultEntities(headers, entityTypes);
       setEntities(defaultEntities);
     }
-  }, []);
+  };
 
   /* Add new entity attribute */
-  const submit = (event: React.FormEvent<HTMLInputElement>): void => {
+  const submit = async (event: React.FormEvent<HTMLInputElement>): Promise<void> => {
     if (!isValidEntities(entities)) {
       return undefined;
     }
     event.preventDefault();
 
     const entity: Entities.ITableEntity = entityFromAttributes(entities);
-    explorer.tableDataClient
-      .createDocument(queryTablesTab.collection, entity)
-      .then((newEntity: Entities.ITableEntity) => {
-        tableEntityListViewModel.addEntityToCache(newEntity).then(() => {
-          if (!tryInsertNewHeaders(tableEntityListViewModel, newEntity)) {
-            tableEntityListViewModel.redrawTableThrottled();
-          }
-        });
-        closePanel();
-      });
+    const newEntity: Entities.ITableEntity = await explorer.tableDataClient.createDocument(
+      queryTablesTab.collection,
+      entity
+    );
+    await tableEntityListViewModel.addEntityToCache(newEntity);
+    if (!tryInsertNewHeaders(tableEntityListViewModel, newEntity)) {
+      tableEntityListViewModel.redrawTableThrottled();
+    }
+    closePanel();
   };
 
   const tryInsertNewHeaders = (viewModel: TableEntityListViewModel, newEntity: Entities.ITableEntity): boolean => {
@@ -260,24 +262,18 @@ export const AddTableEntityPanel: FunctionComponent<AddTableEntityPanelProps> = 
               </Stack>
             )}
           </div>
-          {renderPanelFooter()}
+          <div className="paneFooter">
+            <div className="leftpanel-okbut">
+              <input
+                type="submit"
+                onClick={submit}
+                className="genericPaneSubmitBtn"
+                value={getButtonLabel(userContext.apiType)}
+              />
+            </div>
+          </div>
         </div>
       </form>
-    );
-  };
-
-  const renderPanelFooter = (): JSX.Element => {
-    return (
-      <div className="paneFooter">
-        <div className="leftpanel-okbut">
-          <input
-            type="submit"
-            onClick={submit}
-            className="genericPaneSubmitBtn"
-            value={getButtonLabel(userContext.apiType)}
-          />
-        </div>
-      </div>
     );
   };
 
