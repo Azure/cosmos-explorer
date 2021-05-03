2855bf4c7d40151403a3ec9a8f85808867ea3729
hardiknai-techm <HN00734461@TechMahindra.com>
2021-05-03 09:51:16 +0530

    update test:e2e file name

--- src/Explorer/Panes/SettingsPane/SettingsPane.test.tsx
+++ src/Explorer/Panes/SettingsPane/SettingsPane.test.tsx
@@ -3,8 +3,12 @@ import { shallow } from "enzyme";
 import React from "react";
 import { updateUserContext } from "UserContext";
 import { SettingsPane } from "./SettingsPane";
+
+const props = {
+  expandConsole: (): void => undefined,
   closePanel: (): void => undefined,
 };
+
 describe("Settings Pane", () => {
   it("should render Default properly", () => {
     const wrapper = shallow(<SettingsPane {...props} />);
