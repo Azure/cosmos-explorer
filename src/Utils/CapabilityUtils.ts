2f6dbd83f3124a9ce54bd0b58e61b893c1defd63
victor-meng <56978073+victor-meng@users.noreply.github.com>
2021-05-12 11:56:24 -0700

    Fix throughput input component and add database panel (#773)

--- src/Utils/CapabilityUtils.ts
+++ src/Utils/CapabilityUtils.ts
@@ -1,4 +1,7 @@
+import * as Constants from "../Common/Constants";
 import { userContext } from "../UserContext";
 
 export const isCapabilityEnabled = (capabilityName: string): boolean =>
   userContext.databaseAccount?.properties?.capabilities?.some((capability) => capability.name === capabilityName);
+
+export const isServerlessAccount = (): boolean => isCapabilityEnabled(Constants.CapabilityNames.EnableServerless);
