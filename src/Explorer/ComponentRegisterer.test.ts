f9e8b5eaaae0af2b39b93ab999bcca5f0904a3a8
Steve Faulkner <southpolesteve@gmail.com>
2021-05-13 18:03:29 -0500

    Remove Unused Knockout Components (#783)

--- src/Explorer/ComponentRegisterer.test.ts
+++ src/Explorer/ComponentRegisterer.test.ts
@@ -4,14 +4,6 @@ import * as ko from "knockout";
 import "./ComponentRegisterer";
 
 describe("Component Registerer", () => {
-  it("should register input-typeahead component", () => {
-    expect(ko.components.isRegistered("input-typeahead")).toBe(true);
-  });
-
-  it("should register error-display component", () => {
-    expect(ko.components.isRegistered("error-display")).toBe(true);
-  });
-
   it("should register json-editor component", () => {
     expect(ko.components.isRegistered("json-editor")).toBe(true);
   });
