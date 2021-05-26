63e354874d156c309318bd2aad45865674974bdf
Tanuj Mittal <tamitta@microsoft.com>
2021-05-26 10:57:03 -0700

    Fix lint warning

--- src/Common/DatabaseAccountUtility.ts
+++ src/Common/DatabaseAccountUtility.ts
@@ -12,6 +12,6 @@ function isPrivateEndpointConnectionsEnabled() {
   return userContext.databaseAccount?.properties?.privateEndpointConnections?.length > 0;
 }
 
-export function isPublicInternetAccessAllowed() {
+export function isPublicInternetAccessAllowed(): boolean {
   return !isVirtualNetworkFilterEnabled() && !isIpRulesEnabled() && !isPrivateEndpointConnectionsEnabled();
 }
