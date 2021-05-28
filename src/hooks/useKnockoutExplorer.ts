8ccc81561d14166006cc9a4ca2559817f5d07e11
Zachary Foster <notzachfoster@gmail.com>
2021-05-27 20:17:39 -0400

    format

--- src/hooks/useKnockoutExplorer.ts
+++ src/hooks/useKnockoutExplorer.ts
@@ -15,14 +15,14 @@ import {
   ConnectionString,
   EncryptedToken,
   HostedExplorerChildFrame,
-  ResourceToken
+  ResourceToken,
 } from "../HostedExplorerChildFrame";
 import { emulatorAccount } from "../Platform/Emulator/emulatorAccount";
 import { extractFeatures } from "../Platform/Hosted/extractFeatures";
 import { parseResourceTokenConnectionString } from "../Platform/Hosted/Helpers/ResourceTokenUtils";
 import {
   getDatabaseAccountKindFromExperience,
-  getDatabaseAccountPropertiesFromMetadata
+  getDatabaseAccountPropertiesFromMetadata,
 } from "../Platform/Hosted/HostedUtils";
 import { CollectionCreation } from "../Shared/Constants";
 import { DefaultExperienceUtility } from "../Shared/DefaultExperienceUtility";
@@ -109,7 +109,7 @@ async function configureHostedWithAAD(config: AAD, explorerParams: ExplorerParam
     if (userContext.features.enableAadDataPlane) {
       console.warn(e);
     } else {
-      throw new Error(`List keys failed: ${e.message}`)
+      throw new Error(`List keys failed: ${e.message}`);
     }
   }
   updateUserContext({
