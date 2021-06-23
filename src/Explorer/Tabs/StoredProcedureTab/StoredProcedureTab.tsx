b392bed1b004fb56a165282b4207fee56c65d290
vaidankarswapnil <81285216+vaidankarswapnil@users.noreply.github.com>
2021-06-23 22:19:57 +0530

    Migrate Stored Procedure tab to react (#894)
    Co-authored-by: Steve Faulkner <southpolesteve@gmail.com>

---
+++ src/Explorer/Tabs/StoredProcedureTab/StoredProcedureTab.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { ExecuteSprocResult } from "../../../Common/dataAccess/executeStoredProcedure";
+import * as DataModels from "../../../Contracts/DataModels";
+import * as ViewModels from "../../../Contracts/ViewModels";
+import Explorer from "../../Explorer";
+import StoredProcedure from "../../Tree/StoredProcedure";
+import ScriptTabBase from "../ScriptTabBase";
+import StoredProcedureTabComponent, {
+  IStoredProcTabComponentProps,
+  IStorProcTabComponentAccessor,
+} from "./StoredProcedureTabComponent";
+
+export interface IStoredProcTabProps {
+  container: Explorer;
+  collection: ViewModels.Collection;
+}
+
+export class NewStoredProcedureTab extends ScriptTabBase {
+  public queryText: string;
+  public currentQuery: string;
+  public partitionKey: DataModels.PartitionKey;
+  public iStoredProcTabComponentProps: IStoredProcTabComponentProps;
+  public iStoreProcAccessor: IStorProcTabComponentAccessor;
+  public node: StoredProcedure;
+  public onSaveClick: () => void;
+  public onUpdateClick: () => Promise<void>;
+
+  constructor(options: ViewModels.ScriptTabOption, private props: IStoredProcTabProps) {
+    super(options);
+    this.partitionKey = options.partitionKey;
+
+    this.iStoredProcTabComponentProps = {
+      resource: options.resource,
+      isNew: options.isNew,
+      tabKind: options.tabKind,
+      title: options.title,
+      tabPath: options.tabPath,
+      collectionBase: options.collection,
+      node: options.node,
+      hasLocation: options.hashLocation,
+      scriptTabBaseInstance: this,
+      collection: props.collection,
+      iStorProcTabComponentAccessor: (instance: IStorProcTabComponentAccessor) => {
+        this.iStoreProcAccessor = instance;
+      },
+      container: props.container,
+    };
+  }
+
+  public render(): JSX.Element {
+    return <StoredProcedureTabComponent {...this.iStoredProcTabComponentProps} />;
+  }
+
+  public onTabClick(): void {
+    this.manager?.activateTab(this);
+    this.iStoreProcAccessor.onTabClickEvent();
+  }
+
+  public onCloseTabButtonClick(): void {
+    this.manager?.closeTab(this);
+  }
+
+  public onExecuteSprocsResult(result: ExecuteSprocResult): void {
+    this.iStoreProcAccessor.onExecuteSprocsResultEvent(result);
+  }
+
+  public onExecuteSprocsError(error: string): void {
+    this.iStoreProcAccessor.onExecuteSprocsErrorEvent(error);
+  }
+}
