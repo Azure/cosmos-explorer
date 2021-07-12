9d934898e8a3a3bc1ac2992e462f3a0c1b7adf4e
Tanuj Mittal <tamitta@microsoft.com>
2021-07-12 14:38:06 -0700

    Add tests

--- src/Explorer/Controls/Notebook/NotebookTerminalComponent.test.tsx
+++ src/Explorer/Controls/Notebook/NotebookTerminalComponent.test.tsx
@@ -1,12 +1,87 @@
 import { shallow } from "enzyme";
 import React from "react";
+import * as DataModels from "../../../Contracts/DataModels";
 import { NotebookTerminalComponent, NotebookTerminalComponentProps } from "./NotebookTerminalComponent";
 
+const testAccount: DataModels.DatabaseAccount = {
+  id: "id",
+  kind: "kind",
+  location: "location",
+  name: "name",
+  properties: {
+    documentEndpoint: "https://testDocumentEndpoint.azure.com/",
+  },
+  type: "type",
+};
+
+const testMongo32Account: DataModels.DatabaseAccount = {
+  ...testAccount,
+};
+
+const testMongo36Account: DataModels.DatabaseAccount = {
+  ...testAccount,
+  properties: {
+    mongoEndpoint: "https://testMongoEndpoint.azure.com/",
+  },
+};
+
+const testCassandraAccount: DataModels.DatabaseAccount = {
+  ...testAccount,
+  properties: {
+    cassandraEndpoint: "https://testCassandraEndpoint.azure.com/",
+  },
+};
+
+const testNotebookServerInfo: DataModels.NotebookWorkspaceConnectionInfo = {
+  authToken: "authToken",
+  notebookServerEndpoint: "https://testNotebookServerEndpoint.azure.com",
+};
+
+const testMongoNotebookServerInfo: DataModels.NotebookWorkspaceConnectionInfo = {
+  authToken: "authToken",
+  notebookServerEndpoint: "https://testNotebookServerEndpoint.azure.com/mongo",
+};
+
+const testCassandraNotebookServerInfo: DataModels.NotebookWorkspaceConnectionInfo = {
+  authToken: "authToken",
+  notebookServerEndpoint: "https://testNotebookServerEndpoint.azure.com/cassandra",
+};
+
 describe("NotebookTerminalComponent", () => {
-  it("renders", () => {
+  it("renders terminal", () => {
     const props: NotebookTerminalComponentProps = {
-      databaseAccount: undefined,
-      notebookServerInfo: undefined,
+      databaseAccount: testAccount,
+      notebookServerInfo: testNotebookServerInfo,
+    };
+
+    const wrapper = shallow(<NotebookTerminalComponent {...props} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("renders mongo 3.2 shell", () => {
+    const props: NotebookTerminalComponentProps = {
+      databaseAccount: testMongo32Account,
+      notebookServerInfo: testMongoNotebookServerInfo,
+    };
+
+    const wrapper = shallow(<NotebookTerminalComponent {...props} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("renders mongo 3.6 shell", () => {
+    const props: NotebookTerminalComponentProps = {
+      databaseAccount: testMongo36Account,
+      notebookServerInfo: testMongoNotebookServerInfo,
+    };
+
+    const wrapper = shallow(<NotebookTerminalComponent {...props} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("renders cassandra shell", () => {
+    const props: NotebookTerminalComponentProps = {
+      databaseAccount: testCassandraAccount,
+      notebookServerInfo: testCassandraNotebookServerInfo,
     };
 
     const wrapper = shallow(<NotebookTerminalComponent {...props} />);
