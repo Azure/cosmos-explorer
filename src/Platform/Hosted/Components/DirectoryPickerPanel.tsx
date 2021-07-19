9685e768fa9eb7a366a7b115c4712aa4b654da9d
vaidankarswapnil <swapnil.v@zen3tech.com>
2021-07-19 11:17:17 +0530

    Fixed DirectoryPickerPanel and NotificationConsoleComponent

--- src/Platform/Hosted/Components/DirectoryPickerPanel.tsx
+++ src/Platform/Hosted/Components/DirectoryPickerPanel.tsx
@@ -1,4 +1,4 @@
-import { Panel, PanelType, ChoiceGroup } from "@fluentui/react";
+import { ChoiceGroup, Panel, PanelType } from "@fluentui/react";
 import * as React from "react";
 import { useDirectories } from "../../../hooks/useDirectories";
 
@@ -7,7 +7,7 @@ interface Props {
   dismissPanel: () => void;
   tenantId: string;
   armToken: string;
-  switchTenant: (tenantId: string) => void;
+  switchTenant: (tenantId: string | undefined) => void;
 }
 
 export const DirectoryPickerPanel: React.FunctionComponent<Props> = ({
@@ -29,8 +29,8 @@ export const DirectoryPickerPanel: React.FunctionComponent<Props> = ({
       <ChoiceGroup
         options={directories.map((dir) => ({ key: dir.tenantId, text: `${dir.displayName} (${dir.tenantId})` }))}
         selectedKey={tenantId}
-        onChange={(event, option) => {
-          switchTenant(option.key);
+        onChange={(_event, option) => {
+          switchTenant(option?.key);
           dismissPanel();
         }}
       />
