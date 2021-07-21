932fd0bee59c73a90753cd3c510987bc83dbd0a6
Victor Meng <vimeng@microsoft.com>
2021-07-21 13:43:49 -0700

    add waitForExplorer in upload.spec

--- test/notebooks/upload.spec.ts
+++ test/notebooks/upload.spec.ts
@@ -2,6 +2,7 @@ import { jest } from "@jest/globals";
 import "expect-playwright";
 import fs from "fs";
 import path from "path";
+import { waitForExplorer } from "../utils/waitForExplorer";
 jest.setTimeout(240000);
 
 const filename = "GettingStarted.ipynb";
@@ -10,12 +11,8 @@ const fileToUpload = `GettingStarted-ignore${Math.floor(Math.random() * 100000)}
 fs.copyFileSync(path.join(__dirname, filename), path.join(__dirname, fileToUpload));
 
 test("Notebooks", async () => {
-  page.setDefaultTimeout(50000);
   await page.goto("https://localhost:1234/testExplorer.html?accountName=portal-sql-runner");
-  await page.waitForSelector("iframe");
-  const explorer = page.frame({
-    name: "explorer",
-  });
+  const explorer = await waitForExplorer();
   // Upload and Delete Notebook
   await explorer.click('[data-test="My Notebooks"] [aria-label="More"]');
   await explorer.click('button[role="menuitem"]:has-text("Upload File")');
