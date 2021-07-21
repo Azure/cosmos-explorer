913fec4e69d00ab154d1633d8f4c5466d5fa1cfb
victor-meng <56978073+victor-meng@users.noreply.github.com>
2021-07-21 16:22:31 -0700

    Improve e2e stability (#949)

--- test/tables/container.spec.ts
+++ test/tables/container.spec.ts
@@ -1,22 +1,22 @@
 import { jest } from "@jest/globals";
 import "expect-playwright";
 import { generateUniqueName } from "../utils/shared";
+import { waitForExplorer } from "../utils/waitForExplorer";
 
 jest.setTimeout(120000);
 
 test("Tables CRUD", async () => {
   const tableId = generateUniqueName("table");
+  page.setDefaultTimeout(50000);
 
   await page.goto("https://localhost:1234/testExplorer.html?accountName=portal-tables-runner");
-  await page.waitForSelector("iframe");
-  const explorer = page.frame({
-    name: "explorer",
-  });
+  const explorer = await waitForExplorer();
 
+  await page.waitForSelector('text="Querying databases"', { state: "detached" });
   await explorer.click('[data-test="New Table"]');
   await explorer.fill('[aria-label="Table id"]', tableId);
   await explorer.click("#sidePanelOkButton");
-  await explorer.click(`[data-test="TablesDB"]`, { timeout: 50000 });
+  await explorer.click(`[data-test="TablesDB"]`);
   await explorer.click(`[data-test="${tableId}"] [aria-label="More"]`);
   await explorer.click('button[role="menuitem"]:has-text("Delete Table")');
   await explorer.fill('text=* Confirm by typing the table id >> input[type="text"]', tableId);
