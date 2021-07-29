2e5f02eac53a25ea5dc3c3ef75c8af42c13bcd64
Victor Meng <vimeng@microsoft.com>
2021-07-28 20:37:10 -0700

    Turn off react resource tree

--- src/Platform/Hosted/extractFeatures.ts
+++ src/Platform/Hosted/extractFeatures.ts
@@ -15,7 +15,7 @@ export type Features = {
   readonly enableTtl: boolean;
   readonly executeSproc: boolean;
   readonly enableAadDataPlane: boolean;
-  readonly enableKOResourceTree: boolean;
+  readonly enableReactResourceTree: boolean;
   readonly hostedDataExplorer: boolean;
   readonly junoEndpoint?: string;
   readonly livyEndpoint?: string;
@@ -57,7 +57,7 @@ export function extractFeatures(given = new URLSearchParams(window.location.sear
     enableSDKoperations: "true" === get("enablesdkoperations"),
     enableSpark: "true" === get("enablespark"),
     enableTtl: "true" === get("enablettl"),
-    enableKOResourceTree: "true" === get("enablekoresourcetree"),
+    enableReactResourceTree: "true" === get("enablereactresourcetree"),
     executeSproc: "true" === get("dataexplorerexecutesproc"),
     hostedDataExplorer: "true" === get("hosteddataexplorerenabled"),
     junoEndpoint: get("junoendpoint"),
