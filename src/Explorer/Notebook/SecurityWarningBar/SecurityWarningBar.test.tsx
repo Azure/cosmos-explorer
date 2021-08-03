2e76f0c7ca121132ecb9f7d0d6f646086c8226f7
Tanuj Mittal <tamitta@microsoft.com>
2021-08-03 16:11:06 -0700

    Add a snapshot test

---
+++ src/Explorer/Notebook/SecurityWarningBar/SecurityWarningBar.test.tsx
@@ -0,0 +1,31 @@
+import { shallow } from "enzyme";
+import React from "react";
+import { SecurityWarningBar } from "./SecurityWarningBar";
+
+describe("SecurityWarningBar", () => {
+  it("renders if notebook is untrusted", () => {
+    const wrapper = shallow(
+      <SecurityWarningBar
+        contentRef={"contentRef"}
+        isNotebookUntrusted={true}
+        markNotebookAsTrusted={undefined}
+        saveNotebook={undefined}
+      />
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("renders if notebook is trusted", () => {
+    const wrapper = shallow(
+      <SecurityWarningBar
+        contentRef={"contentRef"}
+        isNotebookUntrusted={false}
+        markNotebookAsTrusted={undefined}
+        saveNotebook={undefined}
+      />
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
