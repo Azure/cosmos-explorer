c33a56cf8aba9604aaea5468a42b841256599854
Meha Kaushik <kaushik.meha@gmail.com>
2021-08-18 14:26:35 -0700

    fixing linting issues

--- src/SelfServe/GraphDedicatedGateway/GraphDedicatedGateway.types.ts
+++ src/SelfServe/GraphDedicatedGateway/GraphDedicatedGateway.types.ts
@@ -2,7 +2,7 @@ export enum Regions {
   NorthCentralUS = "NorthCentralUS",
   WestUS = "WestUS",
   EastUS2 = "EastUS2",
-};
+}
 
 export interface AccountProps {
   regions: Regions;
@@ -10,7 +10,7 @@ export interface AccountProps {
   accountName: string;
   collectionThroughput: number;
   dbThroughput: number;
-};
+}
 
 export type GraphDedicatedGatewayServiceResource = {
   id: string;
