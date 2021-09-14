77d9482ce8b25320577202e466d8fe30fe600ca7
Asier Isayas <aisayas@microsoft.com>
2021-09-14 12:06:40 -0400

    fixed hasFlag test

--- src/Platform/Hosted/extractFeatures.ts
+++ src/Platform/Hosted/extractFeatures.ts
@@ -29,8 +29,8 @@ export type Features = {
   readonly pr?: string;
   readonly showMinRUSurvey: boolean;
   readonly ttl90Days: boolean;
-  readonly mongoProxyEndpoint: string;
-  readonly mongoProxyAPIs: string;
+  readonly mongoProxyEndpoint?: string;
+  readonly mongoProxyAPIs?: string;
   readonly notebooksTemporarilyDown: boolean;
 };
 
