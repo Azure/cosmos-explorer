4ace305b79a568b9905f0a12e49781b0e4c2bdd8
Siddharth Joshi <siddjoshi@microsoft.com>
2021-09-20 15:40:25 -0700

    formatting fix

--- src/SelfServe/SqlX/SqlX.rp.ts
+++ src/SelfServe/SqlX/SqlX.rp.ts
@@ -8,7 +8,7 @@ import {
   FetchPricesResponse,
   RegionsResponse,
   SqlxServiceResource,
-  UpdateDedicatedGatewayRequestParameters
+  UpdateDedicatedGatewayRequestParameters,
 } from "./SqlxTypes";
 
 const apiVersion = "2021-04-01-preview";
@@ -139,7 +139,12 @@ const getGeneralPath = (subscriptionId: string, resourceGroup: string, name: str
 };
 
 export const getRegions = async (): Promise<Array<string>> => {
-  const telemetryData = { feature: "Calculate approximate cost", function: "getRegions", description: "", selfServeClassName: SqlX.name };
+  const telemetryData = {
+    feature: "Calculate approximate cost",
+    function: "getRegions",
+    description: "",
+    selfServeClassName: SqlX.name,
+  };
   const getRegionsTimestamp = selfServeTraceStart(telemetryData);
 
   try {
