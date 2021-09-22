572683324529cf3a863be530fef8cdd474c4ebcc
kcheekuri <kcheekuri@microsoft.com>
2021-09-21 23:31:43 -0400

    Reconnect rename and memory heartbeat change

--- src/Common/Constants.ts
+++ src/Common/Constants.ts
@@ -343,7 +343,7 @@ export enum ConnectionStatusType {
   Connecting = "Connecting",
   Connected = "Connected",
   Failed = "Connection Failed",
-  ReConnect = "Re-Connect",
+  ReConnect = "Reconnect",
 }
 
 export const EmulatorMasterKey =
@@ -355,7 +355,7 @@ export const StyleConstants = require("less-vars-loader!../../less/Common/Consta
 
 export class Notebook {
   public static readonly defaultBasePath = "./notebooks";
-  public static readonly heartbeatDelayMs = 20000;
+  public static readonly heartbeatDelayMs = 60000;
   public static readonly kernelRestartInitialDelayMs = 1000;
   public static readonly kernelRestartMaxDelayMs = 20000;
   public static readonly autoSaveIntervalMs = 120000;
