496f596f385e732e47579bd1b45b9ee5868fafac
Armando Trejo Oliver <ato9000@users.noreply.github.com>
2022-03-25 12:59:18 -0700

    Fix Parent Origin Regex (#1239)
    Not all regex are escaped properly

--- src/Utils/MessageValidation.test.ts
+++ src/Utils/MessageValidation.test.ts
@@ -2,26 +2,28 @@ import { isInvalidParentFrameOrigin, isReadyMessage } from "./MessageValidation"
 
 describe("isInvalidParentFrameOrigin", () => {
   test.each`
-    domain                                                           | expected
-    ${"https://cosmos.azure.com"}                                    | ${false}
-    ${"https://cosmos.azure.us"}                                     | ${false}
-    ${"https://cosmos.azure.cn"}                                     | ${false}
-    ${"https://portal.azure.com"}                                    | ${false}
-    ${"https://portal.azure.us"}                                     | ${false}
-    ${"https://portal.azure.cn"}                                     | ${false}
-    ${"https://portal.microsoftazure.de"}                            | ${false}
-    ${"https://subdomain.portal.azure.com"}                          | ${false}
-    ${"https://subdomain.portal.azure.us"}                           | ${false}
-    ${"https://subdomain.portal.azure.cn"}                           | ${false}
-    ${"https://main.documentdb.ext.azure.com"}                       | ${false}
-    ${"https://main.documentdb.ext.azure.us"}                        | ${false}
-    ${"https://main.documentdb.ext.azure.cn"}                        | ${false}
-    ${"https://main.documentdb.ext.microsoftazure.de"}               | ${false}
-    ${"https://random.domain"}                                       | ${true}
-    ${"https://malicious.cloudapp.azure.com"}                        | ${true}
-    ${"https://malicious.germanycentral.cloudapp.microsoftazure.de"} | ${true}
-    ${"https://maliciousazure.com"}                                  | ${true}
-    ${"https://maliciousportalsazure.com"}                           | ${true}
+    domain                                                              | expected
+    ${"https://cosmos.azure.com"}                                       | ${false}
+    ${"https://cosmos.azure.us"}                                        | ${false}
+    ${"https://cosmos.azure.cn"}                                        | ${false}
+    ${"https://portal.azure.com"}                                       | ${false}
+    ${"https://portal.azure.us"}                                        | ${false}
+    ${"https://portal.azure.cn"}                                        | ${false}
+    ${"https://portal.microsoftazure.de"}                               | ${false}
+    ${"https://subdomain.portal.azure.com"}                             | ${false}
+    ${"https://subdomain.portal.azure.us"}                              | ${false}
+    ${"https://subdomain.portal.azure.cn"}                              | ${false}
+    ${"https://main.documentdb.ext.azure.com"}                          | ${false}
+    ${"https://main.documentdb.ext.azure.us"}                           | ${false}
+    ${"https://main.documentdb.ext.azure.cn"}                           | ${false}
+    ${"https://cosmos-db-dataexplorer-germanycentral.azurewebsites.de"} | ${false}
+    ${"https://main.documentdb.ext.microsoftazure.de"}                  | ${false}
+    ${"https://random.domain"}                                          | ${true}
+    ${"https://malicious.cloudapp.azure.com"}                           | ${true}
+    ${"https://malicious.germanycentral.cloudapp.microsoftazure.de"}    | ${true}
+    ${"https://maliciousazure.com"}                                     | ${true}
+    ${"https://maliciousportalsazure.com"}                              | ${true}
+    ${"https://cosmos-db-dataexplorer-germanycentralAazurewebsites.de"} | ${true}
   `("returns $expected when called with $domain", ({ domain, expected }) => {
     expect(isInvalidParentFrameOrigin({ origin: domain } as MessageEvent)).toBe(expected);
   });
