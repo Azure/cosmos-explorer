da1169d0e23a3db5f825f73289cd0553a9cda6b6
Srinath Narayanan <srnara@microsoft.com>
2022-05-04 03:50:44 +0530

    Added publicgallery flight (#1262)
    * added publicgallery flag
    
    * fixed PR comments

--- src/Platform/Hosted/extractFeatures.ts
+++ src/Platform/Hosted/extractFeatures.ts
@@ -38,6 +38,7 @@ export type Features = {
   phoenixNotebooks?: boolean;
   phoenixFeatures?: boolean;
   notebooksDownBanner: boolean;
+  publicGallery?: boolean;
 };
 
 export function extractFeatures(given = new URLSearchParams(window.location.search)): Features {
