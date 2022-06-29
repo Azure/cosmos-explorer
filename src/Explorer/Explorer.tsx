1a5ff2845e189d92f156840fff11040569a7255f
Bala Lakshmi Narayanasami <balalakshmin@microsoft.com>
2022-06-29 11:16:39 +0530

    Format fix

--- src/Explorer/Explorer.tsx
+++ src/Explorer/Explorer.tsx
@@ -21,7 +21,7 @@ import {
   ContainerConnectionInfo,
   IPhoenixConnectionInfoResult,
   IProvisionData,
-  IResponse
+  IResponse,
 } from "../Contracts/DataModels";
 import * as ViewModels from "../Contracts/ViewModels";
 import { GitHubOAuthService } from "../GitHub/GitHubOAuthService";
