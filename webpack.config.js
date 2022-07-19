3e0694b8d2305e161e25fdd5e54105ae08e9fa70
Bala Lakshmi Narayanasami <balalakshmin@microsoft.com>
2022-07-20 01:34:55 +0530

    Setting webpack compress true

--- webpack.config.js
+++ webpack.config.js
@@ -244,7 +244,7 @@ module.exports = function (_env = {}, argv = {}) {
         new TerserPlugin({
           terserOptions: {
             // These options increase our initial bundle size by ~5% but the builds are significantly faster and won't run out of memory
-            compress: false,
+            compress: true,
             mangle: {
               keep_fnames: true,
               keep_classnames: true,
