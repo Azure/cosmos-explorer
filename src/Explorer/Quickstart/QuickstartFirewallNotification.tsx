0ce10977b2e1335a3f9a03f8edb8161ae6364912
Victor Meng <vimeng@microsoft.com>
2022-10-11 10:53:35 -0700

    Fix screenshot not showing

--- src/Explorer/Quickstart/QuickstartFirewallNotification.tsx
+++ src/Explorer/Quickstart/QuickstartFirewallNotification.tsx
@@ -2,6 +2,7 @@ import { Image, PrimaryButton, Stack, Text } from "@fluentui/react";
 import { sendMessage } from "Common/MessageHandler";
 import { MessageTypes } from "Contracts/ExplorerContracts";
 import React from "react";
+import FirewallRuleScreenshot from "../../../images/firewallRule.png";
 
 export const QuickstartFirewallNotification: React.FC = (): JSX.Element => (
   <Stack style={{ padding: "16px 20px" }}>
@@ -10,7 +11,7 @@ export const QuickstartFirewallNotification: React.FC = (): JSX.Element => (
       (0.0.0.0-255.255.255).
     </Text>
     <Text block>We strongly recommend removing this rule once you finish using the PostgreSQL shell.</Text>
-    <Image style={{ margin: "20px 0" }} src="../../../images/firewallRule.png" />
+    <Image style={{ margin: "20px 0" }} src={FirewallRuleScreenshot} />
     <PrimaryButton
       style={{ width: 150 }}
       onClick={() => sendMessage({ type: MessageTypes.OpenPostgresNetworkingBlade })}
