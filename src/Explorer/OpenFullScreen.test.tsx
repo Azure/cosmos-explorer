1213788f9c7694f093cfbe63735461eae5af411e
Armando Trejo Oliver <ato9000@users.noreply.github.com>
2022-10-20 10:58:37 -0700

    Remove share-link feature (#1345)
    * Remove share-link feature
    
    Cosmos DB supports sharing access to an account data using AAD/RBAC now so this feature is unnecessary.
    
    * Fix format issues
    
    * Fix unit tests
    
    * undo package changes

--- src/Explorer/OpenFullScreen.test.tsx
+++ src/Explorer/OpenFullScreen.test.tsx
@@ -1,17 +1,9 @@
-jest.mock("../hooks/useFullScreenURLs");
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
-import { useFullScreenURLs } from "../hooks/useFullScreenURLs";
 import { OpenFullScreen } from "./OpenFullScreen";
 
 it("renders the correct URLs", () => {
-  (useFullScreenURLs as jest.Mock).mockReturnValue({
-    readWrite: "read and write url",
-    read: "read only url",
-  });
-
   render(<OpenFullScreen />);
-  expect(screen.getByLabelText("Read and Write")).toHaveValue("https://cosmos.azure.com/?key=read and write url");
-  expect(screen.getByLabelText("Read Only")).toHaveValue("https://cosmos.azure.com/?key=read only url");
+  expect(screen.getByText("Open")).toBeDefined();
 });
