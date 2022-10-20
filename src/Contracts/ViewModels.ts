1ce3adff0fa7599fad4c6ca4fe9ee711029d0591
victor-meng <56978073+victor-meng@users.noreply.github.com>
2022-10-19 17:12:41 -0700

    Hide launch quick start button and postgres teaching bubbles if account is a replica (#1344)

--- src/Contracts/ViewModels.ts
+++ src/Contracts/ViewModels.ts
@@ -397,6 +397,7 @@ export interface DataExplorerInputsFrame {
   dataExplorerVersion?: string;
   defaultCollectionThroughput?: CollectionCreationDefaults;
   isPostgresAccount?: boolean;
+  isReplica?: boolean;
   // TODO: Update this param in the OSS extension to remove isFreeTier, isMarlinServerGroup, and make nodes a flat array instead of an nested array
   connectionStringParams?: any;
   flights?: readonly string[];
