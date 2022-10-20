d2ce24a7815fd652adcfe815aa99c9885bb8698f
artrejo <ato9000@users.noreply.github.com>
2022-10-19 18:26:17 -0700

    Fix format issues

--- src/Explorer/OpenFullScreen.tsx
+++ src/Explorer/OpenFullScreen.tsx
@@ -2,13 +2,13 @@ import { PrimaryButton, Stack, Text } from "@fluentui/react";
 import * as React from "react";
 
 export const OpenFullScreen: React.FunctionComponent = () => {
-
   return (
     <>
       <div style={{ padding: "34px" }}>
         <Stack tokens={{ childrenGap: 10 }}>
           <Text>
-            Open this database account in a new browser tab with Cosmos DB Explorer. You can connect using your Microsoft account or a connection string.
+            Open this database account in a new browser tab with Cosmos DB Explorer. You can connect using your
+            Microsoft account or a connection string.
           </Text>
           <Stack horizontal tokens={{ childrenGap: 10 }}>
             <PrimaryButton
