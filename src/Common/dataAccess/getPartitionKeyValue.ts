9f7e392c6f4052d46857de4a8dbd8ac1b90ea5f7
Chuck Skelton <chskelt@microsoft.com>
2022-12-12 18:49:07 -0800

    Refactor to explicitly check for Tables

--- src/Common/dataAccess/getPartitionKeyValue.ts
+++ src/Common/dataAccess/getPartitionKeyValue.ts
@@ -1,11 +1,12 @@
+import { userContext } from "UserContext";
 import DocumentId from "../../Explorer/Tree/DocumentId";
 
 export const getPartitionKeyValue = (documentId: DocumentId) => {
-    if (documentId.partitionKeyProperties === undefined && documentId.partitionKeyValue?.length === 0) {
-        return "";
-    }
-    if (documentId.partitionKeyValue?.length === 0) {
-        return undefined;
-    }
-    return documentId.partitionKeyValue;
-}
+  if (userContext.apiType === "Tables" && documentId.partitionKeyValue?.length === 0) {
+    return "";
+  }
+  if (documentId.partitionKeyValue?.length === 0) {
+    return undefined;
+  }
+  return documentId.partitionKeyValue;
+};
