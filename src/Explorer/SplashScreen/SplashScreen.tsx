e3590b10b971337d755a9719da23f9a53e1b20ea
Craig Boger <bogercraig@microsoft.com>
2023-02-10 19:43:11 +0000

    Moved cdb tv to the tips and learn more column on postgres getting started page.

--- src/Explorer/SplashScreen/SplashScreen.tsx
+++ src/Explorer/SplashScreen/SplashScreen.tsx
@@ -699,11 +699,6 @@ export class SplashScreen extends React.Component<SplashScreenProps> {
         description: "",
       },
       {
-        link: "https://developer.azurecosmosdb.com/tv",
-        title: "Learn the Fundamentals with Azure Cosmos DB TV",
-        description: "",
-      },
-      {
         link: "https://go.microsoft.com/fwlink/?linkid=2207425",
         title: "Build Apps with Python/Java/Django",
         description: "",
@@ -729,6 +724,11 @@ export class SplashScreen extends React.Component<SplashScreenProps> {
 
   private getTipsAndLearnMoreItems(): JSX.Element {
     const items: { link: string; title: string; description: string }[] = [
+      {
+        link: "https://developer.azurecosmosdb.com/tv",
+        title: "Learn the Fundamentals with Azure Cosmos DB TV",
+        description: "",
+      },
       {
         link: "https://go.microsoft.com/fwlink/?linkid=2207226",
         title: "Performance Tuning",
