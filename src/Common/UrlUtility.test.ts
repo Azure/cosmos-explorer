8d9a66e38d591a39a2770eadd3d20b357ab1e227
Senthamil Sindhu <sindhuba@microsoft.com>
2023-03-27 11:49:53 -0700

    Run npm format

--- src/Common/UrlUtility.test.ts
+++ src/Common/UrlUtility.test.ts
@@ -1,49 +1,49 @@
 import * as UrlUtility from "./UrlUtility";
 
 describe("parseDocumentsPath", () => {
-    it("empty resource path", () => {
-        const resourcePath = "";
+  it("empty resource path", () => {
+    const resourcePath = "";
 
-        expect(UrlUtility.parseDocumentsPath(resourcePath)).toEqual({});
-    });
+    expect(UrlUtility.parseDocumentsPath(resourcePath)).toEqual({});
+  });
 
-    it("resourcePath does not begin or end with /", () => {
-        const resourcePath = "localhost/portal/home";
-        const expectedResult = {
-            type: "home",
-            objectBody: {
-                id: "portal",
-                self: "/localhost/portal/home/",
-            },
-        };
+  it("resourcePath does not begin or end with /", () => {
+    const resourcePath = "localhost/portal/home";
+    const expectedResult = {
+      type: "home",
+      objectBody: {
+        id: "portal",
+        self: "/localhost/portal/home/",
+      },
+    };
 
-        expect(UrlUtility.parseDocumentsPath(resourcePath)).toEqual(expectedResult);
-    });
+    expect(UrlUtility.parseDocumentsPath(resourcePath)).toEqual(expectedResult);
+  });
 
-    it("resourcePath length is even", () => {
-        const resourcePath = "/localhost/portal/src/home/";
-        const expectedResult = {
-            type: "src",
-            objectBody: {
-                id: "home",
-                self: resourcePath,
-            },
-        };
+  it("resourcePath length is even", () => {
+    const resourcePath = "/localhost/portal/src/home/";
+    const expectedResult = {
+      type: "src",
+      objectBody: {
+        id: "home",
+        self: resourcePath,
+      },
+    };
 
-        expect(UrlUtility.parseDocumentsPath(resourcePath)).toEqual(expectedResult);
-    });
+    expect(UrlUtility.parseDocumentsPath(resourcePath)).toEqual(expectedResult);
+  });
 
-    it("createUri", () => {
-        const baseUri = "http://foo.com/bar/";
-        const relativeUri = "/index.html";
-        const expectedUri = "http://foo.com/bar/index.html";
+  it("createUri", () => {
+    const baseUri = "http://foo.com/bar/";
+    const relativeUri = "/index.html";
+    const expectedUri = "http://foo.com/bar/index.html";
 
-        expect(UrlUtility.createUri(baseUri, relativeUri)).toEqual(expectedUri);
-    });
+    expect(UrlUtility.createUri(baseUri, relativeUri)).toEqual(expectedUri);
+  });
 
-    it("should throw an error if baseUri is empty", () => {
-        expect(() => {
-            UrlUtility.createUri("", "/home");
-        }).toThrow("baseUri is null or empty");
-    });
+  it("should throw an error if baseUri is empty", () => {
+    expect(() => {
+      UrlUtility.createUri("", "/home");
+    }).toThrow("baseUri is null or empty");
+  });
 });
