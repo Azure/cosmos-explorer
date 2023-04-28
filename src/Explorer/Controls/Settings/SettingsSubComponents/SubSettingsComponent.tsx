712634056e32c64b6fd70315e9bb4210d37a54ea
MokireddySampath <120497218+MokireddySampath@users.noreply.github.com>
2023-04-28 19:18:02 +0530

    Update SubSettingsComponent.tsx

--- src/Explorer/Controls/Settings/SettingsSubComponents/SubSettingsComponent.tsx
+++ src/Explorer/Controls/Settings/SettingsSubComponents/SubSettingsComponent.tsx
@@ -55,18 +55,6 @@ export interface SubSettingsComponentProps {
   onSubSettingsSaveableChange: (isSubSettingsSaveable: boolean) => void;
   onSubSettingsDiscardableChange: (isSubSettingsDiscardable: boolean) => void;
 }
-const stylingforvisuallyhiddenspan = {
-  position: "absolute",
-
-  width: "1px",
-  height: "1px",
-  margin: "-1px",
-  border: 0,
-  padding: 0,
-
-  clip: "rect(0 0 0 0)",
-  overflow: "hidden",
-};
 
 export class SubSettingsComponent extends React.Component<SubSettingsComponentProps> {
   private shouldCheckComponentIsDirty = true;
