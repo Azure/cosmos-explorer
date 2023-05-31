904208ef8ac858447f941dbe87b130fb38c47bf1
Chuck Skelton <chskelt@microsoft.com>
2023-05-30 17:54:58 -0700

    Updating snapshots

--- src/Contracts/DataModels.ts
+++ src/Contracts/DataModels.ts
@@ -165,7 +165,7 @@ export interface Database extends Resource {
   collections?: Collection[];
 }
 
-export interface DocumentId extends Resource { }
+export interface DocumentId extends Resource {}
 
 export interface ConflictId extends Resource {
   resourceId?: string;
