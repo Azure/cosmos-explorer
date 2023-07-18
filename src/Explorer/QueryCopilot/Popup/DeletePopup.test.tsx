0bb2be8a32b2efcdb73a38b7faa411cc2a3ee17d
Predrag Klepic <v-prklepic@microsoft.com>
2023-07-18 16:21:57 +0200

    Improving test coverage

--- src/Explorer/QueryCopilot/Popup/DeletePopup.test.tsx
+++ src/Explorer/QueryCopilot/Popup/DeletePopup.test.tsx
@@ -1,6 +1,5 @@
-import { shallow } from "enzyme";
+import { mount, shallow } from "enzyme";
 import React from "react";
-import { any } from "underscore";
 import { DeletePopup } from "./DeletePopup";
 
 describe("Delete Popup snapshot test", () => {
@@ -8,12 +7,98 @@ describe("Delete Popup snapshot test", () => {
     const wrapper = shallow(
       <DeletePopup
         showDeletePopup={true}
-        setShowDeletePopup={() => any}
-        setQuery={() => any}
-        clearFeedback={() => any}
-        showFeedbackBar={() => any}
+        setShowDeletePopup={() => {}}
+        setQuery={() => {}}
+        clearFeedback={() => {}}
+        showFeedbackBar={() => {}}
       />
     );
+    expect(wrapper.find("Modal").prop("isOpen")).toBeTruthy();
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should not render when showDeletePopup is false", () => {
+    const wrapper = shallow(
+      <DeletePopup
+        showDeletePopup={false}
+        setShowDeletePopup={() => {}}
+        setQuery={() => {}}
+        clearFeedback={() => {}}
+        showFeedbackBar={() => {}}
+      />
+    );
+    expect(wrapper.props().children.props.showDeletePopup).toBeFalsy();
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should call setQuery with an empty string and setShowDeletePopup(false) when delete button is clicked", () => {
+    const setQueryMock = jest.fn();
+    const setShowDeletePopupMock = jest.fn();
+    const wrapper = mount(
+      <DeletePopup
+        showDeletePopup={true}
+        setShowDeletePopup={setShowDeletePopupMock}
+        setQuery={setQueryMock}
+        clearFeedback={() => {}}
+        showFeedbackBar={() => {}}
+      />
+    );
+
+    wrapper.find("PrimaryButton").simulate("click");
+
+    expect(setQueryMock).toHaveBeenCalledWith("");
+    expect(setShowDeletePopupMock).toHaveBeenCalledWith(false);
+  });
+
+  it("should call setShowDeletePopup(false) when close button is clicked", () => {
+    const setShowDeletePopupMock = jest.fn();
+    const wrapper = mount(
+      <DeletePopup
+        showDeletePopup={true}
+        setShowDeletePopup={setShowDeletePopupMock}
+        setQuery={() => {}}
+        clearFeedback={() => {}}
+        showFeedbackBar={() => {}}
+      />
+    );
+
+    wrapper.find("DefaultButton").at(1).simulate("click");
+
+    expect(setShowDeletePopupMock).toHaveBeenCalledWith(false);
+  });
+
+  it("should render the appropriate text content", () => {
+    const wrapper = shallow(
+      <DeletePopup
+        showDeletePopup={true}
+        setShowDeletePopup={() => {}}
+        setQuery={() => {}}
+        clearFeedback={() => {}}
+        showFeedbackBar={() => {}}
+      />
+    );
+
+    const textContent = wrapper.find("Text").map((text) => text.props().children);
+
+    expect(textContent).toEqual([
+      <b>Delete code?</b>,
+      "This will clear the query from the query builder pane along with all comments and also reset the prompt pane",
+    ]);
+  });
+
+  it("should have the correct inline style", () => {
+    const wrapper = shallow(
+      <DeletePopup
+        showDeletePopup={true}
+        setShowDeletePopup={() => {}}
+        setQuery={() => {}}
+        clearFeedback={() => {}}
+        showFeedbackBar={() => {}}
+      />
+    );
+
+    const stackStyle = wrapper.find("Stack[style]").props().style;
+
+    expect(stackStyle).toEqual({ padding: "16px 24px", height: "auto" });
   });
 });
--- src/Explorer/QueryCopilot/SamplePrompts/SamplePrompts.test.tsx
+++ src/Explorer/QueryCopilot/SamplePrompts/SamplePrompts.test.tsx
@@ -1,4 +1,4 @@
-import { shallow } from "enzyme";
+import { mount, shallow } from "enzyme";
 import React from "react";
 import { SamplePrompts, SamplePromptsProps } from "./SamplePrompts";
 
@@ -6,8 +6,8 @@ describe("Sample Prompts snapshot test", () => {
   it("should render properly if isSamplePromptsOpen is true", () => {
     const sampleProps: SamplePromptsProps = {
       isSamplePromptsOpen: true,
-      setIsSamplePromptsOpen: () => undefined,
-      setTextBox: () => undefined,
+      setIsSamplePromptsOpen: () => {},
+      setTextBox: () => {},
     };
 
     const wrapper = shallow(<SamplePrompts sampleProps={sampleProps} />);
@@ -17,10 +17,45 @@ describe("Sample Prompts snapshot test", () => {
   it("should render properly if isSamplePromptsOpen is false", () => {
     const sampleProps: SamplePromptsProps = {
       isSamplePromptsOpen: false,
-      setIsSamplePromptsOpen: () => undefined,
-      setTextBox: () => undefined,
+      setIsSamplePromptsOpen: () => {},
+      setTextBox: () => {},
     };
     const wrapper = shallow(<SamplePrompts sampleProps={sampleProps} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should call setTextBox and setIsSamplePromptsOpen(false) when a button is clicked", () => {
+    const setTextBoxMock = jest.fn();
+    const setIsSamplePromptsOpenMock = jest.fn();
+    const sampleProps: SamplePromptsProps = {
+      isSamplePromptsOpen: true,
+      setIsSamplePromptsOpen: setIsSamplePromptsOpenMock,
+      setTextBox: setTextBoxMock,
+    };
+    const wrapper = mount(<SamplePrompts sampleProps={sampleProps} />);
+
+    wrapper.find("DefaultButton").at(0).simulate("click");
+    expect(setTextBoxMock).toHaveBeenCalledWith("Show me products less than 100 dolars");
+    expect(setIsSamplePromptsOpenMock).toHaveBeenCalledWith(false);
+
+    wrapper.find("DefaultButton").at(3).simulate("click");
+    expect(setTextBoxMock).toHaveBeenCalledWith(
+      "Write a query to return all records in this table created in the last thirty days"
+    );
+    expect(setIsSamplePromptsOpenMock).toHaveBeenCalledWith(false);
+  });
+
+  it("should call setIsSamplePromptsOpen(false) when the close button is clicked", () => {
+    const setIsSamplePromptsOpenMock = jest.fn();
+    const sampleProps: SamplePromptsProps = {
+      isSamplePromptsOpen: true,
+      setIsSamplePromptsOpen: setIsSamplePromptsOpenMock,
+      setTextBox: () => {},
+    };
+    const wrapper = mount(<SamplePrompts sampleProps={sampleProps} />);
+
+    wrapper.find("IconButton").simulate("click");
+
+    expect(setIsSamplePromptsOpenMock).toHaveBeenCalledWith(false);
   });
 });
