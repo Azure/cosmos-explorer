10037d844eff87a3c510069f57f8c16fc812f2fe
Predrag Klepic <60631598+Klepic95@users.noreply.github.com>
2023-07-19 10:13:04 +0200

    [Query Copilot] Improving test coverage (#1539)
    * Improving test coverage
    
    * Not leaving empty functions
    
    * Additional test editing
    
    * Correction of the unit test
    
    * Changes made so the tests work correctly
    
    * removing problematic tests
    
    ---------
    
    Co-authored-by: Predrag Klepic <v-prklepic@microsoft.com>

--- src/Explorer/QueryCopilot/Popup/CopyPopup.test.tsx
+++ src/Explorer/QueryCopilot/Popup/CopyPopup.test.tsx
@@ -1,11 +1,48 @@
+import { IconButton } from "@fluentui/react";
 import { shallow } from "enzyme";
 import React from "react";
-import { any } from "underscore";
 import { CopyPopup } from "./CopyPopup";
 
 describe("Copy Popup snapshot test", () => {
+  const setShowCopyPopupMock = jest.fn();
   it("should render when showCopyPopup is true", () => {
-    const wrapper = shallow(<CopyPopup showCopyPopup={true} setShowCopyPopup={() => any} />);
+    const wrapper = shallow(<CopyPopup showCopyPopup={true} setShowCopyPopup={setShowCopyPopupMock} />);
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.prop("setShowCopyPopup")).toBeUndefined();
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should render when showCopyPopup is false", () => {
+    const wrapper = shallow(<CopyPopup showCopyPopup={false} setShowCopyPopup={setShowCopyPopupMock} />);
+    expect(wrapper.prop("showCopyPopup")).toBeFalsy();
+    expect(wrapper.prop("setShowCopyPopup")).toBeUndefined();
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should call setShowCopyPopup(false) when close button is clicked", () => {
+    const wrapper = shallow(<CopyPopup showCopyPopup={true} setShowCopyPopup={setShowCopyPopupMock} />);
+
+    const closeButton = wrapper.find(IconButton);
+    closeButton.props().onClick?.({} as React.MouseEvent<HTMLButtonElement, MouseEvent>);
+
+    expect(setShowCopyPopupMock).toHaveBeenCalledWith(false);
+  });
+
+  it("should have the correct inline styles", () => {
+    const wrapper = shallow(<CopyPopup showCopyPopup={true} setShowCopyPopup={setShowCopyPopupMock} />);
+
+    const stackStyle = wrapper.find("Stack").first().props().style;
+
+    expect(stackStyle).toEqual({
+      position: "fixed",
+      width: 345,
+      height: 66,
+      padding: 10,
+      gap: 5,
+      top: 75,
+      right: 20,
+      background: "#FFFFFF",
+      boxShadow: "0 2px 6px rgba(0, 0, 0, 0.16)",
+    });
   });
 });
--- src/Explorer/QueryCopilot/SamplePrompts/SamplePrompts.test.tsx
+++ src/Explorer/QueryCopilot/SamplePrompts/SamplePrompts.test.tsx
@@ -3,24 +3,25 @@ import React from "react";
 import { SamplePrompts, SamplePromptsProps } from "./SamplePrompts";
 
 describe("Sample Prompts snapshot test", () => {
-  it("should render properly if isSamplePromptsOpen is true", () => {
-    const sampleProps: SamplePromptsProps = {
-      isSamplePromptsOpen: true,
-      setIsSamplePromptsOpen: () => undefined,
-      setTextBox: () => undefined,
-    };
+  const setTextBoxMock = jest.fn();
+  const setIsSamplePromptsOpenMock = jest.fn();
+  const sampleProps: SamplePromptsProps = {
+    isSamplePromptsOpen: true,
+    setIsSamplePromptsOpen: setIsSamplePromptsOpenMock,
+    setTextBox: setTextBoxMock,
+  };
 
+  it("should render properly if isSamplePromptsOpen is true", () => {
     const wrapper = shallow(<SamplePrompts sampleProps={sampleProps} />);
+
     expect(wrapper).toMatchSnapshot();
   });
 
   it("should render properly if isSamplePromptsOpen is false", () => {
-    const sampleProps: SamplePromptsProps = {
-      isSamplePromptsOpen: false,
-      setIsSamplePromptsOpen: () => undefined,
-      setTextBox: () => undefined,
-    };
+    sampleProps.isSamplePromptsOpen = false;
+
     const wrapper = shallow(<SamplePrompts sampleProps={sampleProps} />);
+
     expect(wrapper).toMatchSnapshot();
   });
 });
