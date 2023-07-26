19cf52353cedd358b4709ff18368169dd699fb9f
MokireddySampath <120497218+MokireddySampath@users.noreply.github.com>
2023-07-26 20:32:04 +0530

    Update PanelContainerComponent.tsx

--- src/Explorer/Panes/PanelContainerComponent.tsx
+++ src/Explorer/Panes/PanelContainerComponent.tsx
@@ -58,7 +58,7 @@ export class PanelContainerComponent extends React.Component<PanelContainerProps
         onDismiss={this.onDissmiss}
         isLightDismiss
         type={PanelType.custom}
-        closeButtonAriaLabel="Close"
+        closeButtonAriaLabel="Closes"
         customWidth={this.props.panelWidth ? this.props.panelWidth : "440px"}
         headerClassName="panelHeader"
         onRenderNavigationContent={this.props.onRenderNavigationContent}
