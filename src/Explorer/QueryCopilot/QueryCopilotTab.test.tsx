bc3cba12044058b8b13c476f9719fa6b3f94140e
Predrag Klepic <v-prklepic@microsoft.com>
2023-07-27 11:21:41 +0200

    snapshots updated

--- src/Explorer/QueryCopilot/QueryCopilotTab.test.tsx
+++ src/Explorer/QueryCopilot/QueryCopilotTab.test.tsx
@@ -1,13 +1,58 @@
 import { shallow } from "enzyme";
-import { useQueryCopilotState } from "hooks/useQueryCopilotState";
+import { QueryCopilotState, useQueryCopilotState } from "hooks/useQueryCopilotState";
 import React from "react";
 import Explorer from "../Explorer";
 import { QueryCopilotTab } from "./QueryCopilotTab";
 
+jest.mock("hooks/useQueryCopilotState");
+
 describe("Query copilot tab snapshot test", () => {
   it("should render with initial input", () => {
-    const queryCopilotState = useQueryCopilotState();
-    const wrapper = shallow(<QueryCopilotTab queryCopilotState={queryCopilotState} explorer={new Explorer()} />);
+    const mockQueryCopilotState: QueryCopilotState = {
+      hideFeedbackModalForLikedQueries: false,
+      userPrompt: "",
+      setUserPrompt: jest.fn(),
+      generatedQuery: "",
+      setGeneratedQuery: jest.fn(),
+      query: "",
+      setQuery: jest.fn(),
+      selectedQuery: "",
+      setSelectedQuery: jest.fn(),
+      isGeneratingQuery: false,
+      setIsGeneratingQuery: jest.fn(),
+      isExecuting: false,
+      setIsExecuting: jest.fn(),
+      likeQuery: false,
+      setLikeQuery: jest.fn(),
+      dislikeQuery: false,
+      setDislikeQuery: jest.fn(),
+      showCallout: false,
+      setShowCallout: jest.fn(),
+      showSamplePrompts: false,
+      setShowSamplePrompts: jest.fn(),
+      queryIterator: undefined,
+      setQueryIterator: jest.fn(),
+      queryResults: undefined,
+      setQueryResults: jest.fn(),
+      errorMessage: "",
+      setErrorMessage: jest.fn(),
+      inputEdited: false,
+      isSamplePromptsOpen: false,
+      setIsSamplePromptsOpen: jest.fn(),
+      showDeletePopup: false,
+      setShowDeletePopup: jest.fn(),
+      showFeedbackBar: false,
+      setShowFeedbackBar: jest.fn(),
+      showCopyPopup: false,
+      setshowCopyPopup: jest.fn(),
+      showErrorMessageBar: false,
+      setShowErrorMessageBar: jest.fn(),
+      resetQueryCopilotStates: jest.fn(),
+    };
+
+    (useQueryCopilotState as jest.Mock).mockReturnValue(mockQueryCopilotState);
+
+    const wrapper = shallow(<QueryCopilotTab queryCopilotState={mockQueryCopilotState} explorer={new Explorer()} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
