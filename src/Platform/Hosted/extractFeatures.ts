879cb089494274cdc5ec7dc24b49bfe747564140
sindhuba <122321535+sindhuba@users.noreply.github.com>
2023-08-08 08:03:42 -0700

    Fix issue with feature flag (#1567)

--- src/Platform/Hosted/extractFeatures.ts
+++ src/Platform/Hosted/extractFeatures.ts
@@ -105,7 +105,7 @@ export function extractFeatures(given = new URLSearchParams(window.location.sear
     enableLegacyMongoShellV2Debug: "true" === get("enablelegacymongoshellv2debug"),
     loadLegacyMongoShellFromBE: "true" === get("loadlegacymongoshellfrombe"),
     enableCopilot: "true" === get("enablecopilot"),
-    enableNPSSurvey: "true" === get("enableNPSSurvey"),
+    enableNPSSurvey: "true" === get("enablenpssurvey"),
   };
 }
 
