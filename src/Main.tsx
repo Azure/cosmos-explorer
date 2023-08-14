1a3ca94efbd7283bc3e72dca890128255585be96
MokireddySampath <120497218+MokireddySampath@users.noreply.github.com>
2023-08-14 21:48:25 +0530

    Bug 2262682:Screen reader is not announcing the updated deleting status information after deleting the container. (#1563)

--- src/Main.tsx
+++ src/Main.tsx
@@ -83,7 +83,7 @@ const App: React.FunctionComponent = () => {
   }
 
   return (
-    <div className="flexContainer">
+    <div className="flexContainer" aria-hidden="false">
       <div id="divExplorer" className="flexContainer hideOverflows">
         <div id="freeTierTeachingBubble"> </div>
         {/* Main Command Bar - Start */}
