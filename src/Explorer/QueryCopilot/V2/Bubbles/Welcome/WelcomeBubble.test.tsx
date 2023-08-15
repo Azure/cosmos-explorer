aaa911b4ff31674d983a40533c7c93c8635d7e18
Darko Radivojevic <v-darkora@microsoft.com>
2023-08-15 14:33:02 +0200

    Fix snapshot

--- src/Explorer/QueryCopilot/V2/Bubbles/Welcome/WelcomeBubble.test.tsx
+++ src/Explorer/QueryCopilot/V2/Bubbles/Welcome/WelcomeBubble.test.tsx
@@ -2,6 +2,9 @@ import { shallow } from "enzyme";
 import React from "react";
 import { WelcomeBubble } from "./WelcomeBubble";
 
+const mockedDate = new Date(2023, 7, 15);
+jest.spyOn(global, "Date").mockImplementation(() => (mockedDate as unknown) as string);
+
 describe("Welcome Bubble snapshot test", () => {
   it("should render ", () => {
     const wrapper = shallow(<WelcomeBubble />);
