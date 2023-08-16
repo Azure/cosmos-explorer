d9802bf82070ea5e841a983634d848e2d368ec71
Predrag Klepic <v-prklepic@microsoft.com>
2023-08-16 15:17:21 +0200

    test name changed

--- src/Explorer/QueryCopilot/V2/Sidebar/QueryCopilotSidebar.test.tsx
+++ src/Explorer/QueryCopilot/V2/Sidebar/QueryCopilotSidebar.test.tsx
@@ -2,7 +2,7 @@ import { shallow } from "enzyme";
 import React from "react";
 import { QueryCopilotSidebar } from "./QueryCopilotSidebar";
 
-describe("Footer snapshot test", () => {
+describe("QueryCopilotSidebar snapshot test", () => {
   it("should render ", () => {
     const wrapper = shallow(<QueryCopilotSidebar />);
     expect(wrapper).toMatchSnapshot();
