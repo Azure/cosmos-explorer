f4645df7f8a518847bb2091cce61d34ba3bd261c
Laurent Nguyen <laurent.nguyen@microsoft.com>
2023-09-26 08:33:45 +0000

    Reformat FabricContracts

--- src/Contracts/FabricContract.ts
+++ src/Contracts/FabricContract.ts
@@ -1,25 +1,25 @@
 export type FabricMessage =
   | {
-    type: "newContainer";
-    databaseName: string;
-  }
+      type: "newContainer";
+      databaseName: string;
+    }
   | {
-    type: "initialize";
-    connectionString: string | undefined;
-  }
+      type: "initialize";
+      connectionString: string | undefined;
+    }
   | {
-    type: "openTab";
-    databaseName: string;
-    collectionName: string | undefined;
-  };
+      type: "openTab";
+      databaseName: string;
+      collectionName: string | undefined;
+    };
 
 export type DataExploreMessage =
   | "ready"
   | {
-    type: number;
-    data: {
-      action: "LoadDatabases";
-      actionModifier: "success" | "start";
-      defaultExperience: "SQL";
+      type: number;
+      data: {
+        action: "LoadDatabases";
+        actionModifier: "success" | "start";
+        defaultExperience: "SQL";
+      };
     };
-  };
