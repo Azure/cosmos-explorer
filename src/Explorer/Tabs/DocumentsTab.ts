2dc6fa5acf1ea65434da2a96d58f5d66434873aa
Laurent Nguyen <laurent.nguyen@microsoft.com>
2023-10-04 14:02:20 +0200

    Auto-select first document in documentsTab

--- src/Explorer/Tabs/DocumentsTab.ts
+++ src/Explorer/Tabs/DocumentsTab.ts
@@ -345,6 +345,22 @@ export default class DocumentsTab extends TabsBase {
     return true;
   }
 
+  /**
+   * Query first page of documents
+   * Select and query first document and display content
+   */
+  private async autoPopulateContent() {
+    // reset iterator
+    this._documentsIterator = this.createIterator();
+    // load documents
+    await this.loadNextPage();
+
+    // Select first document and load content
+    if (this.documentIds().length > 0) {
+      this.documentIds()[0].click();
+    }
+  }
+
   public onShowFilterClick(): Q.Promise<any> {
     this.isFilterCreated(true);
     this.isFilterExpanded(true);
@@ -379,10 +395,7 @@ export default class DocumentsTab extends TabsBase {
     this.documentIds([]);
 
     try {
-      // reset iterator
-      this._documentsIterator = this.createIterator();
-      // load documents
-      await this.loadNextPage();
+      await this.autoPopulateContent();
       // collapse filter
       this.appliedFilter(this.filterContent());
       this.isFilterExpanded(false);
@@ -401,6 +414,11 @@ export default class DocumentsTab extends TabsBase {
     return true;
   };
 
+  /**
+   * TODO Doesn't seem to be used: remove?
+   * @param clickedDocumentId
+   * @returns
+   */
   public onDocumentIdClick(clickedDocumentId: DocumentId): Q.Promise<any> {
     if (this.editorState() !== ViewModels.DocumentExplorerState.noDocumentSelected) {
       return Q();
@@ -619,8 +637,7 @@ export default class DocumentsTab extends TabsBase {
 
     if (!this._documentsIterator) {
       try {
-        this._documentsIterator = this.createIterator();
-        await this.loadNextPage();
+        await this.autoPopulateContent();
       } catch (error) {
         if (this.onLoadStartKey != null && this.onLoadStartKey != undefined) {
           TelemetryProcessor.traceFailure(
