7aa638d518872dd343d12fb978aae28fd8eeff6e
Laurent Nguyen <laurent.nguyen@microsoft.com>
2023-10-10 11:50:38 +0200

    Fix compile error

--- src/Explorer/Tabs/QueryTab/QueryTabComponent.tsx
+++ src/Explorer/Tabs/QueryTab/QueryTabComponent.tsx
@@ -217,16 +217,16 @@ export default class QueryTabComponent extends React.Component<IQueryTabComponen
     if (this._iterator === undefined) {
       this._iterator = this.props.isPreferredApiMongoDB
         ? queryIterator(
-            this.props.collection.databaseId,
-            this.props.viewModelcollection,
-            this.state.selectedContent || this.state.sqlQueryEditorContent,
-          )
+          this.props.collection.databaseId,
+          this.props.viewModelcollection,
+          this.state.selectedContent || this.state.sqlQueryEditorContent,
+        )
         : queryDocuments(
-            this.props.collection.databaseId,
-            this.props.collection.id(),
-            this.state.selectedContent || this.state.sqlQueryEditorContent,
-            { enableCrossPartitionQuery: HeadersUtility.shouldEnableCrossPartitionKey() } as FeedOptions,
-          );
+          this.props.collection.databaseId,
+          this.props.collection.id(),
+          this.state.selectedContent || this.state.sqlQueryEditorContent,
+          { enableCrossPartitionQuery: HeadersUtility.shouldEnableCrossPartitionKey() } as unknown as FeedOptions,
+        );
     }
 
     await this._queryDocumentsPage(firstItemIndex);
