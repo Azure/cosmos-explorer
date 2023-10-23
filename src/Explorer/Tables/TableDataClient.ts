793ae315f43f4de1b1e723fa8613fd995567b4c3
Justin Kolasa (from Dev Box) <justinkolasa@microsoft.com>
2023-10-23 14:59:22 -0400

    fixed undefined condition

--- src/Explorer/Tables/TableDataClient.ts
+++ src/Explorer/Tables/TableDataClient.ts
@@ -147,7 +147,7 @@ export class CassandraAPIDataClient extends TableDataClient {
     let properties = "(";
     let values = "(";
     for (let property in entity) {
-      if (entity[property]._ === "" || undefined) {
+      if (entity[property]._ === "" || entity[property]._ === undefined) {
         continue;
       }
       properties = properties.concat(`${property}, `);
@@ -208,7 +208,7 @@ export class CassandraAPIDataClient extends TableDataClient {
           !originalDocument[property] ||
           newEntity[property]._.toString() !== originalDocument[property]._.toString()
         ) {
-          if (newEntity[property]._.toString() === "" || undefined) {
+          if (newEntity[property]._.toString() === "" || newEntity[property]._.toString() === undefined) {
             continue;
           }
           let propertyQuerySegment = this.isStringType(newEntity[property].$)
