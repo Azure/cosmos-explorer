0c09f0f872f61de7b73301aa89a32aea4a3aceed
Laurent Nguyen <laurent.nguyen@microsoft.com>
2023-11-03 15:00:43 +0000

    Fix format

--- src/Contracts/FabricContract.ts
+++ src/Contracts/FabricContract.ts
@@ -3,44 +3,44 @@ import { AuthorizationToken, MessageTypes } from "./MessageTypes";
 // Fabric to Data Explorer
 export type FabricMessage =
   | {
-    type: "newContainer";
-    databaseName: string;
-  }
+      type: "newContainer";
+      databaseName: string;
+    }
   | {
-    type: "initialize";
-    message: {
-      connectionId: string;
-    };
-  }
+      type: "initialize";
+      message: {
+        connectionId: string;
+      };
+    }
   | {
-    type: "authorizationToken";
-    message: {
-      id: string;
-      error: string | undefined;
-      data: AuthorizationToken | undefined;
-    };
-  }
+      type: "authorizationToken";
+      message: {
+        id: string;
+        error: string | undefined;
+        data: AuthorizationToken | undefined;
+      };
+    }
   | {
-    type: "allResourceTokens";
-    message: {
-      id: string;
-      error: string | undefined;
-      data: FabricDatabaseConnectionInfo | undefined;
+      type: "allResourceTokens";
+      message: {
+        id: string;
+        error: string | undefined;
+        data: FabricDatabaseConnectionInfo | undefined;
+      };
     };
-  };
 
 // Data Explorer to Fabric
 export type DataExploreMessage =
   | "ready"
   | {
-    type: MessageTypes.GetAuthorizationToken;
-    id: string;
-    params: GetCosmosTokenMessageOptions[];
-  }
+      type: MessageTypes.GetAuthorizationToken;
+      id: string;
+      params: GetCosmosTokenMessageOptions[];
+    }
   | {
-    type: MessageTypes.GetAllResourceTokens;
-    id: string;
-  };
+      type: MessageTypes.GetAllResourceTokens;
+      id: string;
+    };
 
 export type GetCosmosTokenMessageOptions = {
   verb: "connect" | "delete" | "get" | "head" | "options" | "patch" | "post" | "put" | "trace";
