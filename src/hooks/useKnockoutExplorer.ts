4fe593e89d9af34a201e52a12dabb1f329d2c78c
Craig Boger (from Dev Box) <bogercraig@microsoft.com>
2023-12-07 16:11:30 -0800

    Testing with endpoint refresh every 5 seconds.

--- src/hooks/useKnockoutExplorer.ts
+++ src/hooks/useKnockoutExplorer.ts
@@ -196,7 +196,6 @@ async function configureHosted(): Promise<Explorer> {
   } else {
     throw new Error(`Unknown hosted config: ${win.hostedConfig}`);
   }
-
   window.addEventListener(
     "message",
     (event) => {
