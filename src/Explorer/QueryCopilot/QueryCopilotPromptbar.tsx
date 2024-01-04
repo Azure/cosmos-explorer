0c9264e8b34e273c99accf0ee22468b738c2c788
MokireddySampath <120497218+MokireddySampath@users.noreply.github.com>
2024-01-04 19:37:16 +0530

    Bug 2819239:Screen reader does not announce the loading information which appears on invoking the 'Send' button. (#1703)
    * Alert has been added and content also added accordign to the loader state
    
    * Snapshot of tests have been updated

--- src/Explorer/QueryCopilot/QueryCopilotPromptbar.tsx
+++ src/Explorer/QueryCopilot/QueryCopilotPromptbar.tsx
@@ -303,6 +303,16 @@ export const QueryCopilotPromptbar: React.FC<QueryCopilotPromptProps> = ({
     resetButtonState();
   };
 
+  const getAriaLabel = () => {
+    if (isGeneratingQuery === null) {
+      return " ";
+    } else if (isGeneratingQuery) {
+      return "Content is loading";
+    } else {
+      return "Content is updated";
+    }
+  };
+
   React.useEffect(() => {
     showTeachingBubble();
     useTabs.getState().setIsQueryErrorThrown(false);
@@ -379,7 +389,9 @@ export const QueryCopilotPromptbar: React.FC<QueryCopilotPromptProps> = ({
           onClick={() => startGenerateQueryProcess()}
           aria-label="Send"
         />
-        {isGeneratingQuery && <Spinner style={{ marginLeft: 8 }} />}
+        <div role="alert" aria-label={getAriaLabel()}>
+          {isGeneratingQuery && <Spinner style={{ marginLeft: 8 }} />}
+        </div>
         {showSamplePrompts && (
           <Callout
             styles={{ root: { minWidth: 400, maxWidth: "70vw" } }}
