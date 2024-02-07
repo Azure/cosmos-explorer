f9bac0152ad25b40b7dd4d3ba2dcba232e0c3b09
Asier Isayas <aisayas@microsoft.com>
2024-02-06 21:02:47 -0500

    limit RU threshold only to NoSQL

--- src/Explorer/Tabs/QueryTab/QueryTabComponent.tsx
+++ src/Explorer/Tabs/QueryTab/QueryTabComponent.tsx
@@ -304,7 +304,7 @@ export default class QueryTabComponent extends React.Component<IQueryTabComponen
     });
 
     let queryOperationOptions: QueryOperationOptions;
-    if (userContext.apiType == "SQL" && ruThresholdEnabled()) {
+    if (userContext.apiType === "SQL" && ruThresholdEnabled()) {
       const ruThreshold: number = getRUThreshold();
       queryOperationOptions = {
         ruCapPerOperation: ruThreshold,
