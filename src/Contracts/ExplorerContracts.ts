9fb407bd9fe5824ecab2b86ba5b02b0a8e320a06
Justin Kolasa (from Dev Box) <justinkolasa@microsoft.com>
2024-02-09 15:45:05 -0500

    Correcting import for MessageTypes

--- src/Contracts/ExplorerContracts.ts
+++ src/Contracts/ExplorerContracts.ts
@@ -1,6 +1,6 @@
-import { MessageTypes } from "Contracts/MessageTypes";
 import * as ActionContracts from "./ActionContracts";
 import * as Diagnostics from "./Diagnostics";
+import { MessageTypes } from "./MessageTypes";
 import * as Versions from "./Versions";
 
 export { ActionContracts, Diagnostics, MessageTypes, Versions };
