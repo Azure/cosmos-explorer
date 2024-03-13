e351689112fcbcaa93ea52a6f8f2d6e401d423f1
Justin Kolasa (from Dev Box) <justinkolasa@microsoft.com>
2024-03-13 11:27:02 -0400

    Enabled the ability to close the home tab

--- src/Explorer/Tabs/Tabs.tsx
+++ src/Explorer/Tabs/Tabs.tsx
@@ -182,11 +182,9 @@ function TabNav({ tab, active, tabKind }: { tab?: Tab; active: boolean; tabKind?
             )}
           </span>
           <span className="tabNavText">{useObservable(tab?.tabTitle || getReactTabTitle())}</span>
-          {tabKind !== ReactTabKind.Home && (
-            <span className="tabIconSection">
-              <CloseButton tab={tab} active={active} hovering={hovering} tabKind={tabKind} />
-            </span>
-          )}
+          <span className="tabIconSection">
+            <CloseButton tab={tab} active={active} hovering={hovering} tabKind={tabKind} />
+          </span>
         </div>
       </span>
     </li>
