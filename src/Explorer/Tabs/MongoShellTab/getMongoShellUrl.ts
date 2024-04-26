002a9e80bb62b1b637d4e3fb31688829167a59a0
Asier Isayas <aisayas@microsoft.com>
2024-04-26 14:31:13 -0400

    open Legacy Mongo SHell with correct base URL in sovereign clouds

--- src/Explorer/Tabs/MongoShellTab/getMongoShellUrl.ts
+++ src/Explorer/Tabs/MongoShellTab/getMongoShellUrl.ts
@@ -1,15 +1,11 @@
-import { configContext } from "ConfigContext";
 import { userContext } from "../../../UserContext";
 
-export function getMongoShellUrl(useMongoProxyEndpoint?: boolean): URL {
+export function getMongoShellUrl(useMongoProxyEndpoint?: boolean): string {
   const { databaseAccount: account } = userContext;
   const resourceId = account?.id;
   const accountName = account?.name;
   const mongoEndpoint = account?.properties?.mongoEndpoint || account?.properties?.documentEndpoint;
   const queryString = `resourceId=${resourceId}&accountName=${accountName}&mongoEndpoint=${mongoEndpoint}`;
-  const path: string = useMongoProxyEndpoint
-    ? `/mongoshell/index.html?${queryString}`
-    : `/mongoshell/indexv2.html?${queryString}`;
 
-  return new URL(path, configContext.hostedExplorerURL);
+  return useMongoProxyEndpoint ? `/mongoshell/index.html?${queryString}` : `/mongoshell/indexv2.html?${queryString}`;
 }
