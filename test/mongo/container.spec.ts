ddddf5507f9d8d12bf76ec03a03c007981e561a7
Laurent Nguyen <laurent.nguyen@microsoft.com>
2024-05-16 20:26:29 +0200

    Revert "Attempt 2 at troubleshooting failing test"
    This reverts commit 3e51a593bf2001d44b6e5316d4865e6e861c21c3.

--- test/mongo/container.spec.ts
+++ test/mongo/container.spec.ts
@@ -40,9 +40,7 @@ test("Mongo CRUD", async () => {
   await explorer.fill('text=* Confirm by typing the collection id >> input[type="text"]', containerId);
   await explorer.click('[aria-label="OK"]');
   await explorer.click(`[data-test="${databaseId}"] [aria-label="More options"]`);
-  await page.screenshot({ path: "beforeDeleteDatabase.png" });
   await explorer.click('button[role="menuitem"]:has-text("Delete Database")');
-  await page.screenshot({ path: "afterDeleteDatabase.png" });
   await explorer.click('text=* Confirm by typing the database id >> input[type="text"]');
   await explorer.fill('text=* Confirm by typing the database id >> input[type="text"]', databaseId);
   await explorer.click("#sidePanelOkButton");
