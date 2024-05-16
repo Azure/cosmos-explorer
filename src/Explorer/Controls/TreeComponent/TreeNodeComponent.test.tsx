3d540224ffaa9cc31a35dfe1921f21dbf9d99893
Ashley Stanton-Nurse <ashleyst@microsoft.com>
2024-05-16 11:33:05 -0700

    move PromiseSource to shared module

--- src/Explorer/Controls/TreeComponent/TreeNodeComponent.test.tsx
+++ src/Explorer/Controls/TreeComponent/TreeNodeComponent.test.tsx
@@ -1,27 +1,9 @@
 import { TreeItem, TreeItemLayout, tokens } from "@fluentui/react-components";
+import PromiseSource from "Utils/PromiseSource";
 import { mount, shallow } from "enzyme";
 import React from "react";
 import { act } from "react-dom/test-utils";
 import { TreeNode, TreeNodeComponent } from "./TreeNodeComponent";
-
-class PromiseSource<T = void> {
-    promise: Promise<T>;
-    resolve: (value: T) => void;
-    reject: (reason?: unknown) => void;
-
-    constructor() {
-        this.promise = new Promise((resolve, reject) => {
-            this.resolve = resolve;
-            this.reject = reject;
-        });
-    }
-
-    /** Resolves the promise, then gets off the thread and waits until the currently-registered 'then' callback run. */
-    async resolveAndWait(value: T): Promise<T> {
-        this.resolve(value);
-        return await this.promise;
-    }
-}
 
 function generateTestNode(id: string, additionalProps?: Partial<TreeNode>): TreeNode {
     const node: TreeNode = {
