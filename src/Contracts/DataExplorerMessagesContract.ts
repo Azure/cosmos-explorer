185643ec905167df4ca32d0e6a09d73b89d7bcfa
Laurent Nguyen <languye@microsoft.com>
2024-05-23 17:27:12 +0200

    Fix format

--- src/Contracts/DataExplorerMessagesContract.ts
+++ src/Contracts/DataExplorerMessagesContract.ts
@@ -6,19 +6,19 @@ export const DATA_EXPLORER_RPC_VERSION = "3";
 // Data Explorer to Fabric
 export type DataExploreMessageV3 =
   | {
-    type: FabricMessageTypes.Ready;
-    id: string;
-    params: [string]; // version
-  }
+      type: FabricMessageTypes.Ready;
+      id: string;
+      params: [string]; // version
+    }
   | {
-    type: FabricMessageTypes.GetAuthorizationToken;
-    id: string;
-    params: GetCosmosTokenMessageOptions[];
-  }
+      type: FabricMessageTypes.GetAuthorizationToken;
+      id: string;
+      params: GetCosmosTokenMessageOptions[];
+    }
   | {
-    type: FabricMessageTypes.GetAllResourceTokens;
-    id: string;
-  };
+      type: FabricMessageTypes.GetAllResourceTokens;
+      id: string;
+    };
 
 export type GetCosmosTokenMessageOptions = {
   verb: "connect" | "delete" | "get" | "head" | "options" | "patch" | "post" | "put" | "trace";
