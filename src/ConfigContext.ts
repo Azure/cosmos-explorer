4539ed5c5d9536586ed1d8d73b3c0609f8cc8911
Asier Isayas <aisayas@microsoft.com>
2024-06-27 14:34:53 -0400

    cleanup

--- src/ConfigContext.ts
+++ src/ConfigContext.ts
@@ -200,7 +200,7 @@ if (process.env.NODE_ENV === "development") {
     PROXY_PATH: "/proxy",
     EMULATOR_ENDPOINT: "https://localhost:8081",
     PORTAL_BACKEND_ENDPOINT: PortalBackendEndpoints.Mpac,
-    // MONGO_PROXY_ENDPOINT: MongoProxyEndpoints.Mpac,
+    MONGO_PROXY_ENDPOINT: MongoProxyEndpoints.Mpac,
     CASSANDRA_PROXY_ENDPOINT: CassandraProxyEndpoints.Mpac,
   });
 }
@@ -255,3 +255,4 @@ export async function initializeConfiguration(): Promise<ConfigContext> {
 }
 
 export { configContext };
+
