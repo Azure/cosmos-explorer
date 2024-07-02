a4fc7bebbef99cc07218dd813d269722d5f5b4e4
Satyapriya Bai <v-satybai@microsoft.com>
2024-07-02 10:50:11 +0530

    updated module structure.

--- src/Explorer/Controls/Tabs/TabComponent.tsx
+++ src/Explorer/Controls/Tabs/TabComponent.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Pivot, PivotItem } from "@fluentui/react/lib/Pivot";
+import { Pivot, PivotItem } from "@fluentui/react";
 import "./TabComponent.less";
 
 export interface TabContent {
