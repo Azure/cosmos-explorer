8c888a751c7bd8cbbe3aabc7b27785485a89abff
Senthamil Sindhu <sindhuba@microsoft.com>
2024-07-08 14:26:04 -0700

    Remove unnecessary code

--- src/Common/CosmosClient.ts
+++ src/Common/CosmosClient.ts
@@ -32,14 +32,6 @@ export const tokenProvider = async (requestInfo: Cosmos.RequestInfo) => {
     console.log(`Returning Auth token`);
     return authorizationToken;
   }
-
-  if ((userContext.dataPlaneRbacEnabled) && userContext.authorizationToken) {
-    console.log(` Getting Portal Auth token `)
-    const AUTH_PREFIX = `type=aad&ver=1.0&sig=`;
-    const authorizationToken = `${AUTH_PREFIX}${userContext.authorizationToken}`;
-    console.log(`Returning Portal Auth token`);
-    return authorizationToken;
-  }
   
   if (configContext.platform === Platform.Emulator) {
     // TODO This SDK method mutates the headers object. Find a better one or fix the SDK.
