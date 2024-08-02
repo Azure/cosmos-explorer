bc479fb8089556f7ca60a4d4953e402fcfbe6cae
Laurent Nguyen <laurent.nguyen@microsoft.com>
2024-08-02 17:53:00 +0200

    Add Word Wrap menu option to Editor React monaco context menu in Documents Tab (#1922)
    * Remove "Go to Symbol..." menu option by default in monaco. Add option to toggle word wrap.
    
    * Remove code that removes "Go to symbol" as it is not a public API
    
    * Move WordWrap context menu item to its own section. Remove unnecessary parameters.
    
    * Fix format

--- src/Explorer/Tabs/DocumentsTabV2/DocumentsTabV2.tsx
+++ src/Explorer/Tabs/DocumentsTabV2/DocumentsTabV2.tsx
@@ -1876,6 +1876,7 @@ export const DocumentsTabComponent: React.FunctionComponent<IDocumentsTabCompone
                     lineNumbers={"on"}
                     theme={"_theme"}
                     onContentChanged={_onEditorContentChange}
+                    enableWordWrapContextMenuItem={true}
                   />
                 )}
                 {selectedRows.size > 1 && (
