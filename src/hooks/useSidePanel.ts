44f0d09bdeba9e197c0259ad14343e613d362a5c
Satyapriya Bai <v-satybai@microsoft.com>
2024-09-24 13:26:56 +0530

    [accessibility-2262594]: [Keyboard Navigation - Azure Cosmos DB - Data Explorer]: Keyboard focus is not retaining back to 'more' button after closing 'Delete container' dialog.

--- src/hooks/useSidePanel.ts
+++ src/hooks/useSidePanel.ts
@@ -7,12 +7,18 @@ export interface SidePanelState {
   headerText?: string;
   openSidePanel: (headerText: string, panelContent: JSX.Element, panelWidth?: string, onClose?: () => void) => void;
   closeSidePanel: () => void;
+  getRef?: React.RefObject<HTMLElement>; // Optional ref for focusing the last element.
 }
-
 export const useSidePanel: UseStore<SidePanelState> = create((set) => ({
   isOpen: false,
   panelWidth: "440px",
   openSidePanel: (headerText, panelContent, panelWidth = "440px") =>
     set((state) => ({ ...state, headerText, panelContent, panelWidth, isOpen: true })),
-  closeSidePanel: () => set((state) => ({ ...state, isOpen: false })),
+  closeSidePanel: () => {
+    set((state) => ({ ...state, isOpen: false }));
+    const lastFocusedElement = useSidePanel.getState().getRef;
+    setTimeout(() => {
+      lastFocusedElement?.current?.focus();
+    }, 300);
+  },
 }));
