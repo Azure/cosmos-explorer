29e5f160650aa0464f70ad3f8d7c7038adece0d6
Craig Boger (from Dev Box) <bogercraig@microsoft.com>
2025-01-17 17:51:29 -0800

    Add logic to reset regional endpoint when global is selected.

--- src/Common/CosmosClient.ts
+++ src/Common/CosmosClient.ts
@@ -120,8 +120,8 @@ export const endpoint = () => {
     return configContext.EMULATOR_ENDPOINT || location.origin;
   }
   return (
-    userContext.endpoint ||
     userContext.selectedRegionalEndpoint ||
+    userContext.endpoint ||
     userContext?.databaseAccount?.properties?.documentEndpoint
   );
 };
