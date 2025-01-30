eba6a31850dbb7038ac42f4aa2c4947f68be7837
Justin Kolasa (from Dev Box) <justinkolasa@microsoft.com>
2025-01-30 09:34:31 -0500

    Added ExplorerBindings back

--- src/hooks/useKnockoutExplorer.ts
+++ src/hooks/useKnockoutExplorer.ts
@@ -53,7 +53,7 @@ import {
 } from "../Utils/AuthorizationUtils";
 import { isInvalidParentFrameOrigin, shouldProcessMessage } from "../Utils/MessageValidation";
 import { getReadOnlyKeys, listKeys } from "../Utils/arm/generatedClients/cosmos/databaseAccounts";
-
+import { applyExplorerBindings } from "../applyExplorerBindings";
 // This hook will create a new instance of Explorer.ts and bind it to the DOM
 // This hook has a LOT of magic, but ideally we can delete it once we have removed KO and switched entirely to React
 // Please tread carefully :)
@@ -88,6 +88,11 @@ export function useKnockoutExplorer(platform: Platform): Explorer {
           restoreOpenTabs();
         }
 
+        useEffect(() => {
+          if (explorer) {
+            applyExplorerBindings(explorer);
+          }
+        }, [explorer]);
         setExplorer(explorer);
       }
     };
