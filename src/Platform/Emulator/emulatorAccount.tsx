692a85219453bb71626e5bfecc4adcf2a7e2c8dc
Nishtha Ahuja <nishthaahuja@microsoft.com>
2025-02-11 14:43:34 +0530

    working changes

--- src/Platform/Emulator/emulatorAccount.tsx
+++ src/Platform/Emulator/emulatorAccount.tsx
@@ -2,4 +2,3 @@ const emulatorConfig = require("./emulatorConfig.json");
 
 export const EmulatorMasterKey = emulatorConfig.EmulatorMasterKey || "";
 export const emulatorAccount = emulatorConfig.Account || {};
-console.log("I am master", EmulatorMasterKey);
