bea3dbe430991d30c7384aa7c41f03c94d116e83
Craig Boger (from Dev Box) <bogercraig@microsoft.com>
2025-02-12 14:49:02 -0800

    Use endpoint instead of region name to track selected region.  Prevents having to do endpoint lookups.

--- src/Common/Constants.ts
+++ src/Common/Constants.ts
@@ -232,10 +232,6 @@ export class SavedQueries {
   public static readonly PartitionKeyProperty: string = "id";
 }
 
-export class RegionSelectionOptions {
-  public static readonly Global: string = "Global";
-}
-
 export class DocumentsGridMetrics {
   public static DocumentsPerPage: number = 100;
   public static IndividualRowHeight: number = 34;
--- src/Shared/StorageUtility.ts
+++ src/Shared/StorageUtility.ts
@@ -11,7 +11,7 @@ export enum StorageKey {
   RUThreshold,
   QueryTimeoutEnabled,
   QueryTimeout,
-  SelectedRegion,
+  SelectedRegionalEndpoint,
   RetryAttempts,
   RetryInterval,
   MaxWaitTimeInSeconds,
