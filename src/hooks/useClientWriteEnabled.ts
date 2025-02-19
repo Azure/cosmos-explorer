d1382dcb555ba84452e9e8c207b42dd47c0848e4
Craig Boger (from Dev Box) <bogercraig@microsoft.com>
2025-02-19 15:45:11 -0800

    Default to write enabled in react.

--- src/hooks/useClientWriteEnabled.ts
+++ src/hooks/useClientWriteEnabled.ts
@@ -1,12 +1,10 @@
 import create, { UseStore } from "zustand";
-import { userContext } from "../UserContext";
-
 interface ClientWriteEnabledState {
   clientWriteEnabled: boolean;
   setClientWriteEnabled: (writeEnabled: boolean) => void;
 }
 
 export const useClientWriteEnabled: UseStore<ClientWriteEnabledState> = create((set) => ({
-  clientWriteEnabled: userContext.writeEnabledInSelectedRegion ?? true,
+  clientWriteEnabled: true,
   setClientWriteEnabled: (clientWriteEnabled: boolean) => set({ clientWriteEnabled }),
 }));
