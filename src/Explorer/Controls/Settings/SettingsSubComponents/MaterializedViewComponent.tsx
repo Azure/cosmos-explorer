8c8f39428b8d54b17d94164f83a3e02b1d163adb
Nishtha Ahuja <nishthaahuja@microsoft.com>
2025-02-21 16:01:07 +0530

    target container

--- src/Explorer/Controls/Settings/SettingsSubComponents/MaterializedViewComponent.tsx
+++ src/Explorer/Controls/Settings/SettingsSubComponents/MaterializedViewComponent.tsx
@@ -2,12 +2,14 @@ import { Link, Stack, Text } from "@fluentui/react";
 import React from "react";
 import * as ViewModels from "../../../../Contracts/ViewModels";
 import { MaterializedViewSourceComponent } from "./MaterializedViewSourceComponent";
+import { MaterializedViewTargetComponent } from "./MaterializedViewTargetComponent";
 
 export interface MaterializedViewComponentProps {
   collection: ViewModels.Collection;
 }
 
 export const MaterializedViewComponent: React.FC<MaterializedViewComponentProps> = ({ collection }) => {
+  const isTargetContainer = !!collection?.materializedViewDefinition();
   const isSourceContainer = !!collection?.materializedViews();
 
   return (
@@ -23,6 +25,7 @@ export const MaterializedViewComponent: React.FC<MaterializedViewComponentProps>
         </Text>
       </Stack>
       {isSourceContainer && <MaterializedViewSourceComponent collection={collection} />}
+      {isTargetContainer && <MaterializedViewTargetComponent collection={collection} />}
     </Stack>
   );
 };
