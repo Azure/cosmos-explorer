1fec734054344c69a2c4bf155d7a7aa4fb6f532e
Nishtha Ahuja <nishthaahuja@microsoft.com>
2025-02-24 15:49:02 +0530

    icon views

--- src/Explorer/Controls/Settings/SettingsSubComponents/MaterializedViewComponent.tsx
+++ src/Explorer/Controls/Settings/SettingsSubComponents/MaterializedViewComponent.tsx
@@ -1,4 +1,4 @@
-import { Link, Stack, Text } from "@fluentui/react";
+import { FontIcon, Link, Stack, Text } from "@fluentui/react";
 import React from "react";
 import * as ViewModels from "../../../../Contracts/ViewModels";
 import { MaterializedViewSourceComponent } from "./MaterializedViewSourceComponent";
@@ -16,10 +16,10 @@ export const MaterializedViewComponent: React.FC<MaterializedViewComponentProps>
     <Stack tokens={{ childrenGap: 8 }} styles={{ root: { maxWidth: 600 } }}>
       <Stack horizontal verticalAlign="center" wrap tokens={{ childrenGap: 8 }}>
         <Text styles={{ root: { fontWeight: 600 } }}>This container has the following views defined for it.</Text>
-        {/* change href */}
         <Text>
-          <Link target="_blank" href="https://aka.ms/MicrosoftCopilotForAzureInCDBHowTo">
+          <Link href="https://learn.microsoft.com/en-us/azure/cosmos-db/nosql/materialized-views#defining-materialized-views">
             Learn more
+            <FontIcon iconName="NavigateExternalInline" style={{ marginLeft: "4px" }} />
           </Link>{" "}
           about how to define materialized views and how to use them.
         </Text>
