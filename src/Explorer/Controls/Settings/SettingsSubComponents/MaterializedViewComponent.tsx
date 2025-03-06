6838bad61f962c8d7de30ed7ad51b4d0db445d4d
Nishtha Ahuja <45535788+nishthaAhujaa@users.noreply.github.com>
2025-03-06 18:59:41 +0530

    Materialized View Target Container (#2065)
    * target container
    
    * icon views
    
    ---------
    
    Co-authored-by: Nishtha Ahuja <nishthaahuja@microsoft.com>

--- src/Explorer/Controls/Settings/SettingsSubComponents/MaterializedViewComponent.tsx
+++ src/Explorer/Controls/Settings/SettingsSubComponents/MaterializedViewComponent.tsx
@@ -1,28 +1,31 @@
-import { Link, Stack, Text } from "@fluentui/react";
+import { FontIcon, Link, Stack, Text } from "@fluentui/react";
 import React from "react";
 import * as ViewModels from "../../../../Contracts/ViewModels";
 import { MaterializedViewSourceComponent } from "./MaterializedViewSourceComponent";
+import { MaterializedViewTargetComponent } from "./MaterializedViewTargetComponent";
 
 export interface MaterializedViewComponentProps {
   collection: ViewModels.Collection;
 }
 
 export const MaterializedViewComponent: React.FC<MaterializedViewComponentProps> = ({ collection }) => {
+  const isTargetContainer = !!collection?.materializedViewDefinition();
   const isSourceContainer = !!collection?.materializedViews();
 
   return (
     <Stack tokens={{ childrenGap: 8 }} styles={{ root: { maxWidth: 600 } }}>
       <Stack horizontal verticalAlign="center" wrap tokens={{ childrenGap: 8 }}>
         <Text styles={{ root: { fontWeight: 600 } }}>This container has the following views defined for it.</Text>
-        {/* change href */}
         <Text>
-          <Link target="_blank" href="https://aka.ms/MicrosoftCopilotForAzureInCDBHowTo">
+          <Link href="https://learn.microsoft.com/en-us/azure/cosmos-db/nosql/materialized-views#defining-materialized-views">
             Learn more
+            <FontIcon iconName="NavigateExternalInline" style={{ marginLeft: "4px" }} />
           </Link>{" "}
           about how to define materialized views and how to use them.
         </Text>
       </Stack>
       {isSourceContainer && <MaterializedViewSourceComponent collection={collection} />}
+      {isTargetContainer && <MaterializedViewTargetComponent collection={collection} />}
     </Stack>
   );
 };
