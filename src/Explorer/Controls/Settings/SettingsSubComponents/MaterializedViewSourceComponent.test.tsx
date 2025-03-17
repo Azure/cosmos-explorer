d788dc935b278052e818e9066fd53ab70619ad73
nishthaAhujaa <nishtha17354@iiittd.ac.in>
2025-03-17 22:34:18 +0530

    updated tests

--- src/Explorer/Controls/Settings/SettingsSubComponents/MaterializedViewSourceComponent.test.tsx
+++ src/Explorer/Controls/Settings/SettingsSubComponents/MaterializedViewSourceComponent.test.tsx
@@ -1,37 +1,34 @@
 import { PrimaryButton } from "@fluentui/react";
 import { shallow } from "enzyme";
 import React from "react";
-import { collection } from "../TestUtils";
-import {
-  MaterializedViewSourceComponent,
-  MaterializedViewSourceComponentProps,
-} from "./MaterializedViewSourceComponent";
+import { collection, container } from "../TestUtils";
+import { MaterializedViewSourceComponent } from "./MaterializedViewSourceComponent";
 
 describe("MaterializedViewSourceComponent", () => {
-  const baseProps: MaterializedViewSourceComponentProps = {
-    collection: {
-      ...collection,
-      materializedViews: jest.fn(() => collection.materializedViews()),
-    },
-  };
+  let testCollection: typeof collection;
+  let testExplorer: typeof container;
+
+  beforeEach(() => {
+    testCollection = { ...collection };
+  });
 
   it("renders without crashing", () => {
-    const wrapper = shallow(<MaterializedViewSourceComponent {...baseProps} />);
+    const wrapper = shallow(<MaterializedViewSourceComponent collection={testCollection} explorer={testExplorer} />);
     expect(wrapper.exists()).toBe(true);
   });
 
   it("renders the PrimaryButton", () => {
-    const wrapper = shallow(<MaterializedViewSourceComponent {...baseProps} />);
+    const wrapper = shallow(<MaterializedViewSourceComponent collection={testCollection} explorer={testExplorer} />);
     expect(wrapper.find(PrimaryButton).exists()).toBe(true);
   });
 
   it("updates when new materialized views are provided", () => {
-    const wrapper = shallow(<MaterializedViewSourceComponent {...baseProps} />);
+    const wrapper = shallow(<MaterializedViewSourceComponent collection={testCollection} explorer={testExplorer} />);
 
     // Simulating an update by modifying the observable directly
-    collection.materializedViews([{ id: "view3", _rid: "rid3" }]);
+    testCollection.materializedViews([{ id: "view3", _rid: "rid3" }]);
 
-    wrapper.setProps({ collection: { ...collection } });
+    wrapper.setProps({ collection: testCollection });
     wrapper.update();
 
     expect(wrapper.find(PrimaryButton).exists()).toBe(true);
