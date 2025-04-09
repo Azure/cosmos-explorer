e98e4ec6043cc4c36a6d719813883c5dfd26ade4
Sourabh Jain <sourabhjain@microsoft.com>
2025-04-09 15:52:20 +0530

    refactored code

--- src/Explorer/Tabs/CloudShellTab/ShellTypes/VCoreMongoShellHandler.tsx
+++ src/Explorer/Tabs/CloudShellTab/ShellTypes/VCoreMongoShellHandler.tsx
@@ -4,47 +4,38 @@
  */
 
 import { userContext } from "../../../../UserContext";
-import { listKeys } from "../../../../Utils/arm/generatedClients/cosmos/databaseAccounts";
-import { getHostFromUrl } from "../Utils/CommonUtils";
-import { ShellTypeConfig } from "./ShellTypeFactory";
+import { AbstractShellHandler } from "./AbstractShellHandler";
 
-export class VCoreMongoShellHandler implements ShellTypeConfig {
+const PACKAGE_VERSION: string = "2.3.8";
+
+export class VCoreMongoShellHandler extends AbstractShellHandler {
 
   public getShellName(): string {
     return "MongoDB VCore";
   }
 
-  public async getInitialCommands(): Promise<string> {
-    const dbAccount = userContext.databaseAccount;
-    const endpoint = dbAccount.properties.vcoreMongoEndpoint;
-    
-    // Get database key
-    const dbName = dbAccount.name;
-    let key = "";
-    if (dbName) {
-      const keys = await listKeys(userContext.subscriptionId, userContext.resourceGroup, dbName);
-      key = keys?.primaryMasterKey || "";
-    }
-
-    const config = {
-      host: getHostFromUrl(endpoint),
-      name: dbAccount.name,
-      password: key,
-      endpoint: endpoint
-    };
-
-    return this.getCommands(config).join("\n").concat("\n");
+  public getEndpoint(): string {
+    return userContext.databaseAccount?.properties?.vcoreMongoEndpoint;
   }
 
-  private getCommands(config: any): string[] {
+  public getSetUpCommands(): string[] {
     return [
       "if ! command -v mongosh &> /dev/null; then echo '⚠️ mongosh not found. Installing...'; fi",
-      "if ! command -v mongosh &> /dev/null; then curl -LO https://downloads.mongodb.com/compass/mongosh-2.3.8-linux-x64.tgz; fi",
-      "if ! command -v mongosh &> /dev/null; then tar -xvzf mongosh-2.3.8-linux-x64.tgz; fi",
-      "if ! command -v mongosh &> /dev/null; then mkdir -p ~/mongosh && mv mongosh-2.3.8-linux-x64/* ~/mongosh/; fi",
+      `if ! command -v mongosh &> /dev/null; then curl -LO https://downloads.mongodb.com/compass/mongosh-${PACKAGE_VERSION}-linux-x64.tgz; fi`,
+      `if ! command -v mongosh &> /dev/null; then tar -xvzf mongosh-${PACKAGE_VERSION}-linux-x64.tgz; fi`,
+      `if ! command -v mongosh &> /dev/null; then mkdir -p ~/mongosh && mv mongosh-${PACKAGE_VERSION}-linux-x64/* ~/mongosh/; fi`,
       "if ! command -v mongosh &> /dev/null; then echo 'export PATH=$HOME/mongosh/bin:$PATH' >> ~/.bashrc; fi",
-      "source ~/.bashrc",
+      "source ~/.bashrc"
+    ];
+  }
+
+  public getConnectionCommands(config: any): string[] {
+    return [
       `read -p "Enter username: " username && mongosh "mongodb+srv://$username:@${config.endpoint}/?authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000" --tls --tlsAllowInvalidCertificates`
     ];
+  }
+
+  public getTerminalSuppressedData(): string {
+    return "Non-Generic MongoDB Shell";
   }
 }
