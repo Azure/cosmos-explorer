d4419ba5dc8e8411779b21b18ac074d3e1f79f7f
Bikram Choudhury <bchoudhury@microsoft.com>
2025-10-15 12:21:42 +0530

    Added Copy Job prerequisites screen

---
+++ src/Explorer/ContainerCopy/Enums/index.ts
@@ -0,0 +1,10 @@
+export enum CopyJobMigrationType {
+    Offline = "offline",
+    Online = "online",
+}
+
+export enum CopyJobMigrationStatus {
+    Pause = "Pause",
+    Resume = "Resume",
+    Cancel = "Cancel",
+}
