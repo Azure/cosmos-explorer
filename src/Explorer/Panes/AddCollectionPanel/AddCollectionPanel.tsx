df76d7e831057350b07e33423538c0f56595656a
Asier Isayas <aisayas@microsoft.com>
2025-10-24 06:29:26 -0700

    addressed comments

--- src/Explorer/Panes/AddCollectionPanel/AddCollectionPanel.tsx
+++ src/Explorer/Panes/AddCollectionPanel/AddCollectionPanel.tsx
@@ -893,6 +893,7 @@ export class AddCollectionPanel extends React.Component<AddCollectionPanelProps,
                       ) => {
                         this.setState({ fullTextPolicy, fullTextIndexes, fullTextPolicyValidated });
                       }}
+                      // Remove when multi language support on container create issue is fixed
                       englishOnly={true}
                     />
                   </Stack>
