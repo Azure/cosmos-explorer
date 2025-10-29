20875185733c83fad29d9b2714c04c633712da26
Bikram Choudhury <bchoudhury@microsoft.com>
2025-10-29 18:36:13 +0530

    show copyjob screen from portal navigation

--- src/Explorer/ContainerCopy/CopyJobUtils.ts
+++ src/Explorer/ContainerCopy/CopyJobUtils.ts
@@ -1,10 +1,21 @@
 import { DatabaseAccount } from "Contracts/DataModels";
 import { CopyJobErrorType } from "./Types";
 
+const azurePortalMpacEndpoint = "https://ms.portal.azure.com/";
+
 export const buildResourceLink = (resource: DatabaseAccount): string => {
   const resourceId = resource.id;
-  // TODO: update "ms.portal.azure.com" based on environment (e.g. for PROD or Fairfax)
-  return `https://ms.portal.azure.com/#resource${resourceId}`;
+  let parentOrigin = window.location.ancestorOrigins?.[0] ?? window.location.origin;
+
+  if (/\/\/localhost:/.test(parentOrigin)) {
+    parentOrigin = azurePortalMpacEndpoint;
+  } else if (/\/\/cosmos\.azure/.test(parentOrigin)) {
+    parentOrigin = parentOrigin.replace("cosmos.azure", "portal.azure");
+  }
+
+  parentOrigin = parentOrigin.replace(/\/$/, "");
+
+  return `${parentOrigin}/#resource${resourceId}`;
 };
 
 export const COSMOS_SQL_COMPONENT = "CosmosDBSql";
