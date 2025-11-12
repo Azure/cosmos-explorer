013ba59ccc6e21f26bdb1825a88dfd53b27ddd5a
Chuck Skelton <chskelt@microsoft.com>
2025-11-11 17:34:44 -0800

    Fixes an issue where tab titles were not truncating when characters used 4 bytes for encoding.

--- src/Utils/StringUtils.test.ts
+++ src/Utils/StringUtils.test.ts
@@ -26,5 +26,20 @@ describe("StringUtils", () => {
       const transformedString: string | undefined = StringUtils.stripSpacesFromString("");
       expect(transformedString).toBe("");
     });
+
+    it("should return the right number of characters regardless of bytes used per character", () => {
+      const ascii = "aaaaaaaaaa";
+      const twoByteCharacters = "ĀĀĀĀĀĀĀĀĀĀ";
+      const threeByteCharacters = "麥麥麥麥麥麥麥麥麥麥";
+      const fourByteCharacters = "𮮐𮮐𮮐𮮐𮮐𮮐𮮐𮮐𮮐𮮐";
+      const mixedByteSizes = "1Ā麥𮮐2Ā麥𮮐3Ā麥𮮐4Ā麥𮮐5Ā麥𮮐";
+
+      expect(StringUtils.substringUtf(ascii, 0, 5)).toBe("aaaaa");
+      expect(StringUtils.substringUtf(twoByteCharacters, 0, 5)).toBe("ĀĀĀĀĀ");
+      expect(StringUtils.substringUtf(threeByteCharacters, 0, 5)).toBe("麥麥麥麥麥");
+      expect(StringUtils.substringUtf(fourByteCharacters, 0, 5)).toBe("𮮐𮮐𮮐𮮐𮮐");
+      expect(StringUtils.substringUtf(mixedByteSizes, 0, 5)).toBe("1Ā麥𮮐2");
+      expect(StringUtils.substringUtf(mixedByteSizes, 4, 4)).toBe("2Ā麥𮮐");
+    });
   });
 });
--- src/Utils/StringUtils.ts
+++ src/Utils/StringUtils.ts
@@ -17,3 +17,39 @@ export function endsWith(stringToTest: string, suffix: string): boolean {
 export function startsWith(stringToTest: string, prefix: string): boolean {
   return stringToTest.indexOf(prefix) === 0;
 }
+
+/**
+ * Returns the input number of characters from a desired string but takes into account characters encoded with different byte sizes.
+ * @param text The text from which to return the subset
+ * @param startChar The starting character from @param text (zero-based)
+ * @param numChars The number of characters to return starting from @param startChar
+ * @returns The resulting slice of characters
+ */
+export const substringUtf = (text: string, startChar: number, numChars: number) => {
+  const encoded = new TextEncoder().encode(text);
+
+  let currentChar = 0;
+  let currentByte = 0;
+  let startByte = 0;
+  for (; currentChar < startChar + numChars; ) {
+    if (currentChar === startChar) {
+      startByte = currentByte;
+    }
+    switch (encoded[currentByte]) {
+      case 196:
+        currentByte = currentByte + 2;
+        break;
+      case 233:
+        currentByte = currentByte + 3;
+        break;
+      case 240:
+        currentByte = currentByte + 4;
+        break;
+      default:
+        currentByte++;
+    }
+    currentChar++;
+  }
+
+  return new TextDecoder().decode(encoded.slice(startByte, currentByte));
+};
