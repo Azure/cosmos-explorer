b1b72cd29316a3af4cf8208af0e60fcb95716635
Bikram Choudhury <bchoudhury@microsoft.com>
2025-11-17 16:37:47 +0530

    grouped permissions and added styles

--- src/Explorer/ContainerCopy/CreateCopyJob/Screens/Components/PopoverContainer.tsx
+++ src/Explorer/ContainerCopy/CreateCopyJob/Screens/Components/PopoverContainer.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/display-name */
-import { DefaultButton, PrimaryButton, Stack, Text } from "@fluentui/react";
+import { DefaultButton, Overlay, PrimaryButton, Spinner, SpinnerSize, Stack, Text } from "@fluentui/react";
+import ContainerCopyMessages from "Explorer/ContainerCopy/ContainerCopyMessages";
 import React from "react";
 
 interface PopoverContainerProps {
@@ -19,17 +20,31 @@ const PopoverContainer: React.FC<PopoverContainerProps> = React.memo(
         tokens={{ childrenGap: 20 }}
         style={{ maxWidth: 450 }}
       >
+        {isLoading && (
+          <Overlay
+            styles={{
+              root: {
+                backgroundColor: "rgba(255,255,255,0.9)",
+                zIndex: 9999,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              },
+            }}
+          >
+            <Spinner
+              size={SpinnerSize.large}
+              label={ContainerCopyMessages.popoverOverlaySpinnerLabel}
+              styles={{ label: { fontWeight: 600 } }}
+            />
+          </Overlay>
+        )}
         <Text variant="mediumPlus" style={{ fontWeight: 600 }}>
           {title}
         </Text>
         <Text>{children}</Text>
         <Stack horizontal tokens={{ childrenGap: 20 }}>
-          <PrimaryButton
-            text={isLoading ? "" : "Yes"}
-            {...(isLoading ? { iconProps: { iconName: "SyncStatusSolid" } } : {})}
-            onClick={onPrimary}
-            disabled={isLoading}
-          />
+          <PrimaryButton text={"Yes"} onClick={onPrimary} disabled={isLoading} />
           <DefaultButton text="No" onClick={onCancel} disabled={isLoading} />
         </Stack>
       </Stack>
