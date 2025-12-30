eff736596f88013913fa8a2edd13dcc69eec6176
Asier Isayas <aisayas@microsoft.com>
2025-12-30 14:09:53 -0500

    reload all databases before loading offers

--- src/Explorer/Tree/Collection.ts
+++ src/Explorer/Tree/Collection.ts
@@ -598,6 +598,7 @@ export default class Collection implements ViewModels.Collection {
   public onSettingsClick = async (): Promise<void> => {
     useSelectedNode.getState().setSelectedNode(this);
     const throughputCap = userContext.databaseAccount?.properties.capacity?.totalThroughputLimit;
+    await this.container.onRefreshResourcesClick();
     throughputCap && throughputCap !== -1 ? await useDatabases.getState().loadAllOffers() : await this.loadOffer();
     // if (throughputCap && throughputCap !== -1) {
     //   try {
