4d1c42f69ecc57845597fa002a86198c248f5f8c
Sung-Hyun Kang <sunghyunkang@microsoft.com>
2026-02-05 18:22:14 -0600

    Added localization build

---
+++ src/Localization/Keys.generated.ts
@@ -0,0 +1,22 @@
+// -----------------------------------------------------------------
+// THIS FILE IS AUTO-GENERATED — DO NOT EDIT BY HAND
+// Regenerate with:  npm run generate:i18n-keys
+// -----------------------------------------------------------------
+export const Keys = {
+  splashScreen: {
+    title: {
+      /** Welcome to Azure Cosmos DB */
+      default: "splashScreen.title.default",
+      /** Welcome to Azure Cosmos DB for PostgreSQL */
+      postgres: "splashScreen.title.postgres",
+      /** Welcome to Azure DocumentDB (with MongoDB compatibility) */
+      vcoreMongo: "splashScreen.title.vcoreMongo",
+    },
+    subtitle: {
+      /** Globally distributed, multi-model database service for any scale */
+      default: "splashScreen.subtitle.default",
+      /** Get started with our sample datasets, documentation, and additional tools. */
+      getStarted: "splashScreen.subtitle.getStarted",
+    },
+  },
+} as const;
