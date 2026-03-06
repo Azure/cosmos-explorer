a106b0ebac4a2c4465bb24499a2632bb1e8194ca
asier-isayas <asier.isayas@gmail.com>
2026-03-06 10:59:35 -0800

    add the aad endpoint to known authorities when setting up MSAL (#2415)
    Co-authored-by: Asier Isayas <aisayas@microsoft.com>

--- src/Utils/AuthorizationUtils.ts
+++ src/Utils/AuthorizationUtils.ts
@@ -58,6 +58,7 @@ export async function getMsalInstance() {
     auth: {
       authority: `${configContext.AAD_ENDPOINT}organizations`,
       clientId: "203f1145-856a-4232-83d4-a43568fba23d",
+      knownAuthorities: [configContext.AAD_ENDPOINT],
     },
   };
 
