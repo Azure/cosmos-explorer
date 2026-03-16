a15620a734ac1825b4965b3fa5a924bdbdc738e2
Sakshi Gupta <sakshig@microsoft.com>
2026-03-16 14:58:04 +0530

    added link on disabled theme toggle button

--- src/Explorer/Menus/CommandBar/ThemeToggleButton.tsx
+++ src/Explorer/Menus/CommandBar/ThemeToggleButton.tsx
@@ -1,8 +1,11 @@
+import { Link, Text } from "@fluentui/react";
 import * as React from "react";
 import MoonIcon from "../../../../images/MoonIcon.svg";
 import SunIcon from "../../../../images/SunIcon.svg";
 import { useThemeStore } from "../../../hooks/useTheme";
 import { CommandButtonComponentProps } from "../../Controls/CommandButton/CommandButtonComponent";
+
+const PORTAL_SETTINGS_URL = "https://learn.microsoft.com/en-us/azure/azure-portal/set-preferences";
 
 export const ThemeToggleButton = (isPortal?: boolean): CommandButtonComponentProps => {
   const [darkMode, setDarkMode] = React.useState(useThemeStore.getState().isDarkMode);
@@ -22,8 +25,23 @@ export const ThemeToggleButton = (isPortal?: boolean): CommandButtonComponentPro
       iconAlt: "Theme Toggle",
       onCommandClick: undefined,
       commandButtonLabel: undefined,
-      ariaLabel: "Dark Mode is managed in Azure Portal Settings.\nOpen settings",
-      tooltipText: "Dark Mode is managed in Azure Portal Settings.\nOpen settings",
+      ariaLabel: "Dark Mode is managed in Azure Portal Settings",
+      tooltipText: undefined,
+      tooltipContent: React.createElement(
+        "div",
+        { style: { padding: "4px 0" } },
+        React.createElement(Text, { block: true, variant: "small" }, "Dark Mode is managed in Azure Portal Settings"),
+        React.createElement(
+          Link,
+          {
+            href: PORTAL_SETTINGS_URL,
+            target: "_blank",
+            rel: "noopener noreferrer",
+            style: { display: "inline-block", marginTop: "4px", fontSize: "12px" },
+          },
+          "Open settings",
+        ),
+      ),
       hasPopup: false,
       disabled: true,
     };
