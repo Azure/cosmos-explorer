f275dd95b7ec8bd7a731a7f33a88353075e83e5b
Jade Welton <jawelton@microsoft.com>
2026-04-24 16:19:09 -0700

    Handcode the token use in tests.

--- test/fx.ts
+++ test/fx.ts
@@ -138,9 +138,75 @@ export async function getTestExplorerUrl(accountType: TestAccount, options?: Tes
   // For now, since we don't test copilot, we can disable the copilot APIs by setting the feature flag to false.
   params.set("feature.enableCopilot", "false");
 
-  const shardIndex = process.env.PLAYWRIGHT_SHARD_INDEX;
+  let nosqlRbacToken: string | undefined;
+  const shardIndex = process.env.PLAYWRIGHT_SHARD_INDEX ?? "";
+  switch(parseInt(shardIndex)) {
+    case 1:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_1_TOKEN;
+      break;
+    case 2:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_2_TOKEN;
+      break;
+    case 3:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_3_TOKEN;
+      break;
+    case 4:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_4_TOKEN;
+      break;
+    case 5:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_5_TOKEN;
+      break;
+    case 6:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_6_TOKEN;
+      break;
+    case 7:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_7_TOKEN;
+      break;
+    case 8:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_8_TOKEN;
+      break;
+    case 9:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_9_TOKEN;
+      break;
+    case 10:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_10_TOKEN;
+      break;
+    case 11:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_11_TOKEN;
+      break;
+    case 12:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_12_TOKEN;
+      break;
+    case 13:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_13_TOKEN;
+      break;
+    case 14:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_14_TOKEN;
+      break;
+    case 15:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_15_TOKEN;
+      break;
+    case 16:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_16_TOKEN;
+      break;
+    case 17:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_17_TOKEN;
+      break;
+    case 18:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_18_TOKEN;
+      break;
+    case 19:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_19_TOKEN;
+      break;
+    case 20:  
+      nosqlRbacToken = process.env.NOSQL_TESTACCOUNT_20_TOKEN;
+      break;
+  };
 
-  const nosqlRbacToken = process.env[`NOSQL_TESTACCOUNT_${shardIndex}_TOKEN`];
+  if (!nosqlRbacToken) {
+    throw new Error(`No NOSQL RBAC token found for shard index ${shardIndex}`);
+  }
+
   const nosqlReadOnlyRbacToken = process.env.NOSQL_READONLY_TESTACCOUNT_TOKEN;
   const nosqlContainerCopyRbacToken = process.env.NOSQL_CONTAINERCOPY_TESTACCOUNT_TOKEN;
   const tableRbacToken = process.env.TABLE_TESTACCOUNT_TOKEN;
