4c9167f5fd9fe30373f509b8da4a08edcd194dc3
Asier Isayas <aisayas@microsoft.com>
2026-08-17 09:41:38 -0700

    Send authorization header for connection string login backend calls

--- src/hooks/usePortalAccessToken.tsx
+++ src/hooks/usePortalAccessToken.tsx
@@ -7,6 +7,7 @@ export async function fetchAccessData(portalToken: string): Promise<AccessInputM
   const headers = new Headers();
   // Portal encrypted token API quirk: The token header must be URL encoded
   headers.append(HttpHeaders.guestAccessToken, encodeURIComponent(portalToken));
+  headers.append(HttpHeaders.authorization, encodeURIComponent(portalToken));
   const url: string = `${configContext.PORTAL_BACKEND_ENDPOINT}/api/connectionstring/runtimeproxy/accessinputmetadata`;
   const options = {
     method: "GET",
