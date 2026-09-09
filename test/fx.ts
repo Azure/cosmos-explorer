fa688803708ccd90ac104df295ecb8d521233703
Asier Isayas <aisayas@microsoft.com>
2026-09-09 07:33:46 -0700

    Handle Mongo firewall account in test explorer
    Treat the Mongo connection-string firewall test account as a connection-string-only account in the shared E2E URL helper.

--- test/fx.ts
+++ test/fx.ts
@@ -265,6 +265,7 @@ export async function getTestExplorerUrl(accountType: TestAccount, options?: Tes
 
     case TestAccount.SQLConnectionString:
     case TestAccount.SQLConnectionStringPublicNetworkAccessDisabled:
+    case TestAccount.MongoConnectionStringPublicNetworkAccessDisabled:
     case TestAccount.TableConnectionString:
     case TestAccount.GremlinConnectionString:
       // Connection string (account key) login navigates directly to hostedExplorer.html and doesn't
